0f42f1cc87845f639518bc515f401052dc5f36fc
Chris Bond <chris@dcbond.com>
2024-05-16 13:35:09 -0400

    zsh updates

--- system/configuration.nix
+++ system/configuration.nix
@@ -40,7 +40,7 @@
 
   environment.systemPackages = with pkgs; [ # search system packages with 'nix search [package]'
     wget
-    #zsh # z-shell installed system-wide to source necessary files
+    zsh # z-shell installed system-wide to source necessary files
     neovim
     git
     usbutils # package that provides 'lsusb' tool to see usb peripherals plugged in
@@ -51,7 +51,7 @@
       initialPassword = "changeme";
       extraGroups = ["wheel"];
       isNormalUser = true;
-      #shell = pkgs.zsh # user-specific z-shell configs in home.nix
+      shell = pkgs.zsh; # user-specific z-shell configs in home.nix
       #useDefaultShell = true; ?
       openssh.authorizedKeys.keys = [ 
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJZBJOhg+DeRoH1UljG6FniW66qtYVmJNYtreg54WL3 chris@dcbond.com"
@@ -87,7 +87,7 @@
 
   programs.mtr.enable = true;
 
-  #programs.zsh.enable = true; # z-shell enabled system-wide to source necessary files
+  programs.zsh.enable = true; # z-shell enabled system-wide to source necessary files
   #environment.pathsToLink = [ "/share/zsh" ]; # to enable z-shell completion for system packages like systemd
 
   programs.ssh = {
