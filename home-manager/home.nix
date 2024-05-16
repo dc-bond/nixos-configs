a04679a60e21c94705fb3d17468ae4107065e134
Chris Bond <chris@dcbond.com>
2024-05-16 18:20:45 -0400

    updates

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -25,18 +25,37 @@
   home.packages = with pkgs; [
     htop
     eza # modern replacement for 'ls'
+    pfetch # system info on shell startup
     glances
   ];
 
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    #autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
     shellAliases = {
-      #ls = "ls -alh";
       ls = "eza -all --long -g -h --color=always --group-directories-first --git";
-      rebuild = "sudo nixos-rebuild switch --flake /home/chris/nixos-configs";
+      vim = "nvim";
+
+      update = "sudo nixos-rebuild switch --flake ~/nixos-configs";
+      #flake-update = "nix flake update ~/nixos-configs/flake.nix";
+      #upgrade = "sudo nixos-rebuild switch --upgrade --flake ~/nixos-configs#thinkpad";
+
+      configsys = "nvim ~/nixos-configs/system/configuration.nix";
+      confighome = "nvim ~/nixos-configs/home-manager/home.nix";
+
+      addconfig = "cd ~/nixos-configs && git add -A && cd ~";
+      pushconfig = "cd ~/nixos-configs && git add -A && git commit && git push --repo https://github.com/dc-bond/nixos-configs.git && cd ~";
+    };
+    initExtra = ''
+      pfetch
+      # eval "$(starship init zsh)"
+    '';
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "marlonrichert/zsh-autocomplete"; }
+      ];
     };
     history.size = 5000;
     history.path = "${config.xdg.dataHome}/zsh/history";
