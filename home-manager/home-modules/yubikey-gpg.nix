44979873f7f5c3aa4e20b82186441eaba23efb28
Chris Bond <chris@dcbond.com>
2024-05-17 15:53:38 -0400

    updates

--- home-manager/home-modules/yubikey-gpg.nix
+++ home-manager/home-modules/yubikey-gpg.nix
@@ -14,7 +14,7 @@
   ];
 
   programs.zsh = {
-    shellInit = ''
+    initExtra = ''
       export GPG_TTY="$(tty)"
       export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
       gpgconf --launch gpg-agent
