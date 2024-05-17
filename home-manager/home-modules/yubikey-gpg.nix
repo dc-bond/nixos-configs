fc1e9800499c609a61dfe205d4d1d9ee2b5dcac5
Chris Bond <chris@dcbond.com>
2024-05-17 12:51:22 -0400

    gpg updates

--- home-manager/home-modules/yubikey-gpg.nix
+++ home-manager/home-modules/yubikey-gpg.nix
@@ -18,6 +18,24 @@
   programs.gpg = {
     enable = true;
     homedir = "${config.xdg.dataHome}/gnupg";
+    settings = {
+      default-key = "A8DD4B51A93E2D9C15B4D27F0419FDA34202A683"
+      #no-greeting
+      #armor
+      #comment ""
+      #no-emit-version
+      #no-comments
+      keyid-format = "0xlong"
+      #with-fingerprint
+      list-options = "show-uid-validity"
+      verify-options = "show-uid-validity"
+      #use-agent
+      keyserver = "hkps://keyserver.ubuntu.com"
+      personal-cipher-preferences = "AES256 TWOFISH AES192 AES"
+      personal-digest-preferences = "SHA512 SHA384 SHA256 SHA224"
+      cert-digest-algo = "SHA512"
+      default-preference-list = "SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed"
+    }
   };
   
   #services.pcscd.enable = true;
