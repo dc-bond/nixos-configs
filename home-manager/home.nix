e9615a379cc85c9f04f99a8bef146a8b7c57c088
Chris Bond <chris@dcbond.com>
2024-05-18 06:42:42 -0400

    updates

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -159,7 +159,7 @@
     enable = true;
     homedir = "${config.home.homeDirectory}/.gnupg";
     publicKeys = [ 
-      { source = ../"chris@dcbond.com-gpg.pub"; trust = 5; }
+      { source = "../chris@dcbond.com-gpg.pub"; trust = 5; }
     ];
     settings = {
       use-agent = true; # to enable smartcard/ssh support?
