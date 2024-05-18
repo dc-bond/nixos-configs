7e058188986d03e875e6fa51d2d90b784542c4d1
Chris Bond <chris@dcbond.com>
2024-05-17 20:19:06 -0400

    adding pass

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -31,6 +31,13 @@
     glances # system monitor tool
   ];
 
+  programs.passwordStore = {
+    enable = true
+    settings = {
+      PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -116,58 +123,6 @@
       };
     };
   };
-
-  #programs.starship.enable = true;
-  #programs.starship.enableZshIntegration = true;
-  #programs.starship.settings = {
-  #  add_newline = true;
-  #  line_break = {
-  #    disabled = true;
-  #  };
-  #  package = {
-  #    disabled = false;
-  #  };
-  #  username = {
-  #    style_user = "bold yellow";
-  #    style_root = "bold red";
-  #    format = "[$user](style)@";
-  #    disabled = false;
-  #    show_always = true;
-  #  };
-  #  hostname = {
-  #    disabled = false;
-  #    ssh_only = false;
-  #    format = "[$hostname](bold blue): ";
-  #  };
-  #  nix_shell = {
-  #    symbol = " ";
-  #    style = "bold bright-purple";
-  #  };
-  #  docker_context = {
-  #    symbol = " ";
-  #    style = "blue bold";
-  #  };
-  #  python = {
-  #    symbol = " ";
-  #    style = "green bold";
-  #    pyenv_version_name = false;
-  #    python_binary = "python3";
-  #  };
-  #  directory = {
-  #    #read_only = "";
-  #    truncation_length = 10;
-  #    truncate_to_repo = false;
-  #    format = "[$path]($style)[$lock_symbol]($lock_style) ";
-  #  };
-  #  cmd_duration = {
-  #    format = "took [$duration]($style) ";
-  #    min_time = 10000;  # show command duration over 10,000 milliseconds (10 sec)
-  #  };
-  #  character = {
-  #    success_symbol = "[➜](bold green) ";
-  #    error_symbol = "[✖](bold red) ";
-  #  };
-  #};
 
   #services.ssh-agent.enable = true; # default is false, comment out if using gpg-agent to serve ssh
   programs.ssh = {
