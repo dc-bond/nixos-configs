fe18948f07b80a78bd4fca10290bad1f42b3b772
Chris Bond <chris@dcbond.com>
2024-05-19 13:42:22 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -30,33 +30,3 @@
     };
   };
 }
-
-#  outputs = { 
-#    self, 
-#    nixpkgs, 
-#    home-manager, 
-#    ... 
-#  }:
-#    let
-#      system = "x86_64-linux";
-#      lib = nixpkgs.lib;
-#      pkgs = nixpkgs.legacyPackages.${system};
-#    in {
-#      nixosConfigurations = {
-#        thinkpad = lib.nixosSystem {
-#          inherit system;
-#          modules = [ 
-#            ./nixos/configuration.nix 
-#          ];
-#        };
-#      };
-#      homeConfigurations = {
-#        chris = home-manager.lib.homeManagerConfiguration {
-#          inherit pkgs;
-#          modules = [ 
-#            ./home-manager/home.nix 
-#          ];
-#        };
-#      };
-#    };
-#}
--- system/configuration.nix
+++ system/configuration.nix
@@ -12,15 +12,16 @@
   imports = [
     ./hardware-configuration.nix
     #inputs.sops-nix.nixosModules.sops # import sops module
-    inputs.home-manager.nixosModules.home-manager # import home-manager module
+    inputs.home-manager.nixosModules.home-manager # import home-manager module declared in flake.nix
   ];
 
-# ?
+# allow configuration options for packages from the nixpkgs repo
   nixpkgs = {
     overlays = [ # override default packages in nixpkgs repo, e.g. older versions, custom patched, etc.
     ];
     config = {
-      allowUnfree = true; # allow packages marked as proprietary/unfree nixpkgs to be installed
+      allowUnfree = true; # allow packages marked as proprietary/unfree
+      allowBroken = false; # do not allow packages marked as broken
     };
   };
 
@@ -36,6 +37,14 @@
       experimental-features = "nix-command flakes";
       flake-registry = "";
       nix-path = config.nix.nixPath;
+    };
+  };
+
+# ?
+  home-manager = {
+    extraSpecialArgs = { inherit inputs outputs; };
+    users = {
+      chris = import ../home-manager/home.nix;
     };
   };
 
@@ -106,14 +115,6 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJZBJOhg+DeRoH1UljG6FniW66qtYVmJNYtreg54WL3 chris@dcbond.com"
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDesi3Wba5w6/ZV0kgO4hCcG+n7cDwMuSGca/pCqW4zNlCA95Yd9enkQIAtJfUuXbMjZI7DPezcCptDMySUIBU+Lc3WKScJUsaAUjQCSAEv8E1mq6/qg2p2/0GSyl9NONE1iMlASiq8M/q04CL9E7SD6XJCKtqdAOP4mPi5+xzUJ85tvBlyeF8fTsDGQeUSkMm/N31zuymx9lIgf7KQ7bbV0L5Z5R7cSoGs2NrZDnhMpqFYVCh4LA/hhHg7ed8DE96xSJ6GUnulGVa1C8kCVa/fbU1tNBXfOBCooh7yL1MDGAyseAQC4g2ThwWR9Fpyy23Mn9hrr6tuoZ9lwji5RpthuHOYFey82kaDa50yop2BWwN3yXDZjnWJB6Eo8VrGql9o/WytjRh7YvMCC30jAEHEH8IVYGIT14zO9bC5CCCoP6wonkGjhlhdYJFKPQPKZ6X+bESXaC6+3FXY7CsiI/mWxjc5fdJVQRXZDrZaPwhvt292aSZCTY0sDcFwn8HeOO8= openpgp:0xE5DCB627"
         ];
-    };
-  };
-
-# ?
-  home-manager = {
-    extraSpecialArgs = { inherit inputs outputs; };
-    users = {
-      chris = import ../home-manager/home.nix;
     };
   };
 
