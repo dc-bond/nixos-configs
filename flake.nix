2126f67a1d89d4133b9368af760ac8998b484e62
Chris Bond <chris@dcbond.com>
2024-05-19 12:47:32 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -1,31 +1,30 @@
 {
   description = "thinkpad laptop flake";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11"; # update to new channel identifier to update?
+  inputs = { # information about sources/inputs to the flake
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; #  for current stable repo
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # for cutting-edge repo
     #sops-nix.url = "github:Mic92/sops-nix";
     #sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs"; # use version of nixpkgs defined above instead of home-manager's default to avoid getting out of sync
   };
 
-  outputs = {
+  outputs = { # information about what the flake should create with the sources/inputs
     self,
     nixpkgs,
     home-manager,
     ...
   } @ inputs: 
-  
   let
-    system = "x86_64-linux";
     lib = nixpkgs.lib;
     inherit (self) outputs;
   in {
-    nixosConfigurations = {
-      thinkpad = lib.nixosSystem {
+    nixosConfigurations = { # output set that contains details on a nixos system configuration
+      thinkpad = lib.nixosSystem { # if hostname of system is same as 'thinkpad' defined here, no need to specify #thinkpad with the 'nixos-rebuild switch --flake .' command
         specialArgs = {inherit inputs outputs;};
         modules = [
-          ./system/configuration.nix
+          ./system/configuration.nix # nixos system configuration module is in effect the configuration.nix file
         ];
       };
     };
