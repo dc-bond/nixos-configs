a2d6f438e15b479883895b527d7cd20b77dd5fac
Chris Bond <chris@dcbond.com>
2024-05-19 13:30:53 -0400

    update

--- flake.nix
+++ flake.nix
@@ -31,36 +31,6 @@
   };
 }
 
-#{
-#  description = "thinkpad laptop flake";
-#
-#  inputs = {
-#    nixpkgs.url = "nixpkgs/nixos-23.11"; # update to new channel identifier to update?
-#    #sops-nix.url = "github:Mic92/sops-nix";
-#    #sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-#    home-manager.url = "github:nix-community/home-manager/release-23.11";
-#    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # use version of nixpkgs defined above instead of home-manager's default to avoid getting out of sync
-#  };
-#
-#  outputs = {
-#    self,
-#    nixpkgs,
-#    home-manager,
-#    ...
-#  } @ inputs: let
-#    inherit (self) outputs;
-#  in {
-#    nixosConfigurations = {
-#      thinkpad = nixpkgs.lib.nixosSystem {
-#        specialArgs = {inherit inputs outputs;};
-#        modules = [
-#          ./system/configuration.nix
-#        ];
-#      };
-#    };
-#  };
-#}
-
 #  outputs = { 
 #    self, 
 #    nixpkgs, 
