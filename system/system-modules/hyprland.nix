37536ee5c4465ef5c06f28f561b6eb55f8a2c085
Chris Bond <chris@dcbond.com>
2024-05-21 12:29:02 -0400

    udpates

--- system/system-modules/hyprland.nix
+++ system/system-modules/hyprland.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: 
+#{ pkgs, config, ... }: # for nvidia?
 
 {
 
@@ -17,7 +18,7 @@
     #})
     #)
     #eww-wayland # widgets
-    swww # animated wallpaper for wayland window managers
+    #swww # animated wallpaper for wayland window managers
     #swaylock-effects # wayland screenlock application
     #wlogout # wayland logout application
     #nwg-look # gtk settings manager for wayland
@@ -96,10 +97,34 @@
 #    #WLR_NO_HARDWARE_CURSORS = "1"; # if cursor does not appear
 #  };
 
-## hardware
-#  hardware = {
-#    opengl.enable = true; # when using QEMU KVM
-#    nvidia.modesetting.enable = true;
+## nvidia-specific settings
+#  hardware.opengl = {
+#    enable = true;
+#    driSupport = true;
+#    driSupport32Bit = true;
+#  };
+#  services.xserver.videoDrivers = ["nvidia"]; # load nvidia driver for xorg and wayland
+#  hardware.nvidia = {
+#    modesetting.enable = true;
+#    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+#    # Enable this if you have graphical corruption issues or application crashes after waking
+#    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+#    # of just the bare essentials.
+#    powerManagement.enable = false;
+#    # Fine-grained power management. Turns off GPU when not in use.
+#    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+#    powerManagement.finegrained = false;
+#    # Use the NVidia open source kernel module (not to be confused with the
+#    # independent third-party "nouveau" open source driver).
+#    # Support is limited to the Turing and later architectures. Full list of 
+#    # supported GPUs is at: 
+#    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+#    # Only available from driver 515.43.04+
+#    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+#    open = false;
+#    nvidiaSettings = true; # enable nvidia settings menu, accessible via 'nvidia-settings'
+#    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+#    package = config.boot.kernelPackages.nvidiaPackages.stable;
 #  };
 
 }
