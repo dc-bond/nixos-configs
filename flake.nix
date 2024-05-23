42110b63913e8706aed47290b0e9201acba5ffff
Chris Bond <chris@dcbond.com>
2024-05-23 19:32:01 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "thinkpad laptop system configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # for cutting-edge repo
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # for cutting-edge repo
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; # stable repo
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs"; # home-manager sources nixpkgs for its own use so make home-manager use the same version of nixpkgs defined above to avoid getting out of sync
@@ -13,7 +14,6 @@
   let
     system = "x86_64-linux";
     lib = nixpkgs.lib;
-    #pkgs = nixpkgs.legacyPackages.${system};
     pkgs = import nixpkgs { inherit system; };
   in {
     nixosConfigurations = {
