82c34f8e4f81b247e5c71fec78fb8529fd2241d3
Chris Bond <chris@dcbond.com>
2024-05-23 20:48:59 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -2,25 +2,25 @@
   description = "thinkpad laptop system configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; # stable repo
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # for cutting-edge repo
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; 
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     #home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # home-manager sources nixpkgs for its own use so make home-manager use the same version of nixpkgs defined above to avoid getting out of sync
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     #sops-nix.url = "github:Mic92/sops-nix";
     #sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... } @ inputs:
   let
-    lib = nixpkgs.lib; # specify nixpkgs version of lib
+    lib = nixpkgs.lib;
     inherit (self) outputs;
   in {
-    nixosConfigurations = { # output set that contains details on one or more system configurations
-      thinkpad = lib.nixosSystem { # specify 'thinkpad' as system configuration name
-        specialArgs = {inherit inputs outputs;}; # thinkpad system configuration inherits the inputs and outputs of this flake
+    nixosConfigurations = {
+      thinkpad = lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
         modules = [
-          ./system/configuration.nix # nixos system configuration module is in effect the configuration.nix file
+          ./system/configuration.nix
         ];
       };
     };
