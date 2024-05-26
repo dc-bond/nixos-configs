a003f3d653998db0dde6896d1aa4a4430466c029
Chris Bond <chris@dcbond.com>
2024-05-26 06:47:24 -0400

    updates

--- system/configuration.nix
+++ system/configuration.nix
@@ -45,8 +45,6 @@
 # settings for home-manager module
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs; };
-    #useGlobalPkgs = true;
-    #useUserPackages = true;
     users = {
       chris = import ../home/chris/home.nix;
     };
