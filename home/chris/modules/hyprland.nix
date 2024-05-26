0f79fb9dd1c6281f1b126cab823d0e83b7d9061b
Chris Bond <chris@dcbond.com>
2024-05-26 09:46:59 -0400

    updates

--- home/chris/modules/hyprland.nix
+++ home/chris/modules/hyprland.nix
@@ -2,6 +2,15 @@
 
 {
 
+#  home.file = {
+#    ".config/hypr/hyprland.conf".source = ./dotfiles/hyprland.conf;
+#  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+  };
+
   home.packages = with pkgs; [
     waybar
     #(pkgs.waybar.overrideAttrs (oldAttrs: {
@@ -38,37 +47,32 @@
     ##xorg-xset # tool to set keyboard repeat delay
     #bleachbit # disk cleaner
   ];
-
-  wayland.windowManager.hyprland.enable = true;
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
-  };
-
-#  home.file = {
-#    ".config/hypr/hyprland.conf".source = ./dotfiles/hyprland.conf;
-#  };
   
+  #wayland.windowManager.hyprland.settings = {
+  #  "$mod" = "SUPER";
+  #  bind =
+  #    [
+  #      "$mod, F, exec, firefox"
+  #      ", Print, exec, grimblast copy area"
+  #    ]
+  #    ++ (
+  #      # workspaces
+  #      # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+  #      builtins.concatLists (builtins.genList (
+  #          x: let
+  #            ws = let
+  #              c = (x + 1) / 10;
+  #            in
+  #              builtins.toString (x + 1 - (c * 10));
+  #          in [
+  #            "$mod, ${ws}, workspace, ${toString (x + 1)}"
+  #            "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+  #          ]
+  #        )
+  #        10)
+  #    );
+  #};
+
 ## alacritty terminal
 #  programs.alacritty = {
 #    enable = true;
