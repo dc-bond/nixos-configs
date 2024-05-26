1cce3b93d76ae5e6c32999f72f2d61dde6545130
Chris Bond <chris@dcbond.com>
2024-05-26 10:34:57 -0400

    updates

--- home/chris/modules/hyprland.nix
+++ home/chris/modules/hyprland.nix
@@ -4,16 +4,21 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    settings = {
-      "$mainMod" = "Alt";
-      bind = [
-        "$mainMod, RETURN, exec, alacritty"
-        "$mainMod, Q, killactive"
-        ];
-      extraConfig = ''
-        
-      '';
-    };
+    xwayland.enable = false;
+    #settings = {
+    #  "$mainMod" = "Alt";
+    #  bind = [
+    #    "$mainMod, RETURN, exec, alacritty"
+    #    "$mainMod, Q, killactive"
+    #    ];
+    #};
+    extraConfig = ''
+      # keybindings
+      $mainMod = Alt
+      bind = $mainMod, RETURN, exec, alacritty
+      bind = $mainMod, Q, killactive
+      
+    '';
   };
 
   home.packages = with pkgs; [
@@ -53,31 +58,6 @@
     #bleachbit # disk cleaner
   ];
   
-  #wayland.windowManager.hyprland.settings = {
-  #  "$mod" = "SUPER";
-  #  bind =
-  #    [
-  #      "$mod, F, exec, firefox"
-  #      ", Print, exec, grimblast copy area"
-  #    ]
-  #    ++ (
-  #      # workspaces
-  #      # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-  #      builtins.concatLists (builtins.genList (
-  #          x: let
-  #            ws = let
-  #              c = (x + 1) / 10;
-  #            in
-  #              builtins.toString (x + 1 - (c * 10));
-  #          in [
-  #            "$mod, ${ws}, workspace, ${toString (x + 1)}"
-  #            "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-  #          ]
-  #        )
-  #        10)
-  #    );
-  #};
-
 # alacritty terminal
   programs.alacritty = {
     enable = true;
