47504a5a03e09a18b591b206abe6be0ded033e9c
Chris Bond <chris@dcbond.com>
2024-05-28 20:15:49 -0400

    updates

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -2,7 +2,7 @@
 
 {
   
-  imports = [ ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
