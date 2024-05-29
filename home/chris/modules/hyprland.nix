2400a236de83fa568bed10344c5097bafa5b5992
Chris Bond <chris@dcbond.com>
2024-05-29 18:56:45 -0400

    updates

--- home/chris/modules/hyprland.nix
+++ home/chris/modules/hyprland.nix
@@ -10,10 +10,165 @@
 
     #};
     extraConfig = ''
+      # ----------------------------------------------------- 
+      # startup
+      # ----------------------------------------------------- 
       exec-once = swww-daemon
       exec-once = swww img ~/nixos-configs/home/chris/wallpaper/wallpaper-1.jpg
+      # ----------------------------------------------------- 
+      # key bindings
+      # ----------------------------------------------------- 
       $mainMod = Alt
       bind = $mainMod, RETURN, exec, alacritty
+      bind = $mainMod, Q, killactive
+      bind = $mainMod, F, fullscreen
+      bind = $mainMod, D, exec, rofi -show combi -combi-modes "drun,run,ssh" -modes combi
+      bind = $mainMod, C, exec, rofi -show calc -modi calc -no-show-match -no-sort
+      bind = $mainMod, T, togglefloating
+      bind = $mainMod, J, togglesplit
+      bind = $mainMod, left, movefocus, l
+      bind = $mainMod, right, movefocus, r
+      bind = $mainMod, up, movefocus, u
+      bind = $mainMod, down, movefocus, d
+      bind = $mainMod, PRINT, exec, ~/cypress-dotfiles/scripts/screenshot.sh
+      bind = $mainMod SHIFT, Q, exec, ~/cypress-dotfiles/scripts/powermenu.sh
+      bind = $mainMod SHIFT, R, exec, ~/cypress-dotfiles/scripts/updatewal-swww.sh
+      bind = $mainMod, F8, exec, rfkill toggle wlan
+      #bind = $mainMod, F10, exec, rfkill toggle bluetooth
+      bind = $mainMod, F5, exec, brightnessctl set 10%-
+      bind = $mainMod, F6, exec, brightnessctl set +10%
+      bind = $mainMod, 1, workspace, 1
+      bind = $mainMod, 2, workspace, 2
+      bind = $mainMod, 3, workspace, 3
+      bind = $mainMod, 4, workspace, 4
+      bind = $mainMod, 5, workspace, 5
+      bind = $mainMod, 6, workspace, 6
+      bind = $mainMod, 7, workspace, 7
+      bind = $mainMod, 8, workspace, 8
+      bind = $mainMod, 9, workspace, 9
+      bind = $mainMod, 0, workspace, 10
+      bind = $mainMod SHIFT, 1, movetoworkspace, 1
+      bind = $mainMod SHIFT, 2, movetoworkspace, 2
+      bind = $mainMod SHIFT, 3, movetoworkspace, 3
+      bind = $mainMod SHIFT, 4, movetoworkspace, 4
+      bind = $mainMod SHIFT, 5, movetoworkspace, 5
+      bind = $mainMod SHIFT, 6, movetoworkspace, 6
+      bind = $mainMod SHIFT, 7, movetoworkspace, 7
+      bind = $mainMod SHIFT, 8, movetoworkspace, 8
+      bind = $mainMod SHIFT, 9, movetoworkspace, 9
+      bind = $mainMod SHIFT, 0, movetoworkspace, 10
+      bind = $mainMod, mouse_down, workspace, e+1
+      bind = $mainMod, mouse_up, workspace, e-1
+      bind = $mainMod SHIFT, right, resizeactive, 100 0
+      bind = $mainMod SHIFT, left, resizeactive, -100 0
+      bind = $mainMod SHIFT, up, resizeactive, 0 -100
+      bind = $mainMod SHIFT, down, resizeactive, 0 100
+      bindm = $mainMod, mouse:272, movewindow
+      bindm = $mainMod, mouse:273, resizewindow
+      # ----------------------------------------------------- 
+      # environment
+      # ----------------------------------------------------- 
+      env = XCURSOR_SIZE,16
+      #env = PATH,$PATH:$HOME/cypress-dotfiles/scripts:$HOME/.cargo/bin
+      env = EDITOR,nvim
+      env = VISUAL=nvim
+      env = TERM=xterm-256color
+      # ----------------------------------------------------- 
+      # keyboard layout and mouse
+      # ----------------------------------------------------- 
+      input {
+          kb_layout = us
+          kb_variant =
+          kb_model =
+          kb_options =
+          kb_rules =
+          follow_mouse = 1
+          sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+          touchpad {
+              natural_scroll = true
+          }
+      }
+      # ----------------------------------------------------- 
+      # window layout and colors
+      # ----------------------------------------------------- 
+      general {
+          gaps_in = 0
+          gaps_out = 0
+          border_size = 1
+          col.active_border = $color11
+          col.inactive_border = rgba(ffffffff)
+          layout = dwindle
+      }
+      # ----------------------------------------------------- 
+      # window decorations
+      # ----------------------------------------------------- 
+      decoration {
+          rounding = 5
+          blur {
+              enabled = true
+              size = 6
+              passes = 2
+              new_optimizations = on
+              ignore_opacity = true
+              xray = true
+              blurls = waybar
+          }
+          active_opacity = 1.0
+          inactive_opacity = 0.8
+          fullscreen_opacity = 1.0
+      
+          drop_shadow = true
+          shadow_range = 30
+          shadow_render_power = 3
+          col.shadow = 0x66000000
+      }
+      # ----------------------------------------------------- 
+      # animations
+      # ----------------------------------------------------- 
+      animations {
+          enabled = true
+          bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+          animation = windows, 1, 7, myBezier
+          animation = windowsOut, 1, 7, default, popin 80%
+          animation = border, 1, 10, default
+          animation = borderangle, 1, 8, default
+          animation = fade, 1, 7, default
+          animation = workspaces, 1, 6, default
+      }
+      # ----------------------------------------------------- 
+      # layouts
+      # ----------------------------------------------------- 
+      dwindle {
+          pseudotile = true
+          preserve_split = true
+      }
+      master {
+          new_is_master = true
+      }
+      gestures {
+          workspace_swipe = false
+      }
+      # ----------------------------------------------------- 
+      # window rules
+      # ----------------------------------------------------- 
+      windowrule = float,^(pavucontrol)$
+      windowrule = float,^(blueman-manager)$
+      windowrule = float,^(iwdgui)$
+      # ----------------------------------------------------- 
+      # misc settings
+      # ----------------------------------------------------- 
+      misc {
+          disable_hyprland_logo = true
+          disable_splash_rendering = true
+      }
+      input {
+          repeat_delay = 200
+          repeat_rate = 40
+      }
+      #device {
+      #    name = razer-proclickm-1
+      #    sensitivity = -0.7 
+      #}
     '';
   };
 
