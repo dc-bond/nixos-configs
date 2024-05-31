c62418963c7ec09a79c1dd9bd0e4c19837f0d794
Chris Bond <chris@dcbond.com>
2024-05-31 15:42:55 -0400

    updates

--- home/chris/modules/rofi.nix
+++ home/chris/modules/rofi.nix
@@ -12,6 +12,11 @@
     ];
     extraConfig = {
       modes = "window,drun,run,ssh,combi,calc,power-menu:${pkgs.rofi-power-menu}/bin/rofi-power-menu";
+      display-combi = "Combination Mode";
+      display-run = "Shell Scripts";
+      display-drun = "Applications";
+      display-ssh = "SSH";
+      display-calc = "Calculator";
     };
   };
 
