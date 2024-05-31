bce3a5269dcdc96b0bfff3207f66d642368eabbb
Chris Bond <chris@dcbond.com>
2024-05-31 15:36:02 -0400

    updates

--- home/chris/modules/rofi.nix
+++ home/chris/modules/rofi.nix
@@ -2,9 +2,17 @@
 
 {
 
-## rofi
-#  programs.rofi = {
-#    enable = true;
-#  };
+# rofi
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+    plugins = with pkgs; [
+      rofi-calc
+      rofi-power-menu
+    ];
+    extraConfig = {
+      modes = "window,drun,run,ssh,combi,calc,power-menu:${pkgs.rofi-power-menu}/bin/rofi-power-menu";
+    };
+  };
 
 }
