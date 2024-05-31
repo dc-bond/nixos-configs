72c6b1e5e2efb42a4f226232f2cdc102393b13b7
Chris Bond <chris@dcbond.com>
2024-05-31 19:59:56 -0400

    updates

--- home/chris/modules/rofi.nix
+++ home/chris/modules/rofi.nix
@@ -11,11 +11,11 @@
       rofi-power-menu
     ];
     terminal = "${pkgs.alacritty}/bin/alacritty";
-    font = "SauceCodePro Nerd Font 10";
-    location = "center";
+    #font = "SauceCodePro Nerd Font 10";
+    #location = "center";
     theme = ../dotfiles/rofi/Ayu-Mirage.rasi;
     #xoffset = ;
-    yoffset = 29;
+    #yoffset = 29;
     extraConfig = {
       modes = "window,drun,run,ssh,combi,calc,power-menu:${pkgs.rofi-power-menu}/bin/rofi-power-menu";
       display-combi = "Combination Mode";
@@ -24,6 +24,15 @@
       display-ssh = "SSH";
       display-calc = "Calculator";
     };
+    window = {
+      "font" = mkLiteral "SauceCodePro Nerd Font 10";
+      "location" = mkLiteral "center";
+      "border" = mkLiteral "0";
+      "border-radius" = mkLiteral "0";
+      "padding" = mkLiteral "16 14";
+      "width" = mkLiteral "500px";
+      "y-offset" = mkLiteral "29";
+    }
   };
 
 }
