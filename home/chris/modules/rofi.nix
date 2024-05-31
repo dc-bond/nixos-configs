1171816d3cdfc8aef064a44c7e6db011880f43d7
Chris Bond <chris@dcbond.com>
2024-05-31 15:49:14 -0400

    update

--- home/chris/modules/rofi.nix
+++ home/chris/modules/rofi.nix
@@ -10,6 +10,8 @@
       rofi-calc
       rofi-power-menu
     ];
+    font = "SauceCodePro Nerd Font 10";
+    location = "center";
     extraConfig = {
       #modes = "window,drun,run,ssh,combi,calc,power-menu:${pkgs.rofi-power-menu}/bin/rofi-power-menu";
       display-combi = "Combination Mode";
