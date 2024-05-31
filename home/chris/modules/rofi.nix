a4bf45c48ae5d04d922b1cc3dd885188579e919f
Chris Bond <chris@dcbond.com>
2024-05-31 15:59:35 -0400

    updates

--- home/chris/modules/rofi.nix
+++ home/chris/modules/rofi.nix
@@ -21,6 +21,10 @@
       display-drun = "Applications";
       display-ssh = "SSH";
       display-calc = "Calculator";
+      border = 0;
+      border-radius = 0;
+      padding = 16 14;
+      width = 500px;
     };
   };
 
