c8a5e7309c138f9b9c23ea292f8ccc81ddff36c5
dc-bond <chris@dcbond.com>
2024-06-03 19:57:32 -0400

    updates

--- home/chris/home.nix
+++ home/chris/home.nix
@@ -70,6 +70,7 @@
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
       asvetliakov.vscode-neovim
+      arcticicestudio.nord-visual-studio-code
       redhat.vscode-yaml
       pkief.material-icon-theme
       signageos.signageos-vscode-sops
