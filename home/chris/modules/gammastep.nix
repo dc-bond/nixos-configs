673a93052b887ffb38eaf91c7f3dae3da145c198
dc-bond <chris@dcbond.com>
2024-06-13 19:59:33 -0400

    updates

--- home/chris/modules/gammastep.nix
+++ home/chris/modules/gammastep.nix
@@ -8,9 +8,19 @@
     latitude = 40.46;
     longitude = -79.95;
     temperature.day = 6500;
-    temperature.night = 3900;
-    dawnTime = "4:00-7:45";
-    duskTime = "18:30-20:00";
+    temperature.night = 4200;
+    dawnTime = "07:45";
+    duskTime = "19:00";
+    settings = {
+      general = {
+        adjustment-method = "wayland";
+        gamma = 0.8;
+        location-provider = "manual";
+        brightness-day = 1.0;
+        brightness-night = 0.9;
+        fade = 1;
+      };
+    };
   };
 
 }
