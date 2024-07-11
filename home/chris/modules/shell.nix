7befe6dfd1abbe71a00a70d311a6da54298e5b05
dc-bond <chris@dcbond.com>
2024-07-11 13:26:44 -0400

    updates

--- home/chris/modules/shell.nix
+++ home/chris/modules/shell.nix
@@ -12,9 +12,9 @@
     '';
     shellAliases = {
       ls = "eza -all --long -g -h --color=always --group-directories-first --git";
-      rebuild = "sudo nixos-rebuild switch --flake ~/nixos-configs";
+      #rebuild = "sudo nixos-rebuild switch --flake ~/nixos-configs";
       update = "sudo nix flake update ~/nixos-configs";
-      overhaul = "sudo nix flake update ~/nixos-configs && sudo nixos-rebuild switch --flake ~/nixos-configs";
+      overhaul = "sudo nix flake update ~/nixos-configs && rebuild";
       wgup = "sudo networkctl up wg0";
       wgdn = "sudo networkctl down wg0";
       wglogon = "echo module wireguard +p | sudo tee /sys/kernel/debug/dynamic_debug/control";
--- scripts/rebuild-diff.nix
+++ scripts/rebuild-diff.nix
@@ -1,9 +1,9 @@
 { pkgs, config }:
 
-pkgs.writeShellScriptBin "rebuild-diff" 
+pkgs.writeShellScriptBin "rebuild" 
 ''
   nixos_old_gen=$(readlink -f /run/current-system)
   sudo nixos-rebuild switch --flake ~/nixos-configs
   nixos_new_gen=$(readlink -f /run/current-system)
-  nvd "$nixos_old_gen" "$nixos_new_gen"
+  nvd diff "$nixos_old_gen" "$nixos_new_gen"
 ''
