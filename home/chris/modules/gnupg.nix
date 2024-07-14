db68c9e0d153be20e6905e3de32359db5dcba832
dc-bond <chris@dcbond.com>
2024-07-14 06:16:30 -0400

    updates

--- home/chris/modules/gnupg.nix
+++ home/chris/modules/gnupg.nix
@@ -36,12 +36,12 @@
     };
   };
 
-  services.ssh-agent.enable = false; 
+  services.ssh-agent.enable = false; # ensure ssh-agent is not running
   
   services.gpg-agent = {
-    enable = true; # this setting adds export GPG_TTY lines to user's .zshrc
-    enableSshSupport = true; # this setting adds 'gpg-connect-agent updatestartuptty /bye' to user's .zshrc
-    sshKeys = [ # adds keygrip identifier to .gnupg/sshcontrol file
+    enable = true; # this setting adds export GPG_TTY lines to user's .zshrc and starts the agent on login
+    enableSshSupport = true; # this setting adds 'gpg-connect-agent updatestartuptty /bye' to user's .zshrc to replace ssh-agent SSH_AUTH_SOCK with gpg-agent instead
+    sshKeys = [ # adds keygrip identifier to .gnupg/sshcontrol file and load gpg auth private key into gpg-agent
       "DB9ADBBE6FBD1F0E694AF25D012321D46E090E61"
     ];
     enableZshIntegration = true;
