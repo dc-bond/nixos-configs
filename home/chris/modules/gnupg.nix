483257e9e09119d9ac57df46c878c42e2656bcfd
dc-bond <chris@dcbond.com>
2024-09-05 13:35:41 -0400

    updates

--- home/chris/modules/gnupg.nix
+++ home/chris/modules/gnupg.nix
@@ -36,28 +36,17 @@
     };
   };
 
-  services.ssh-agent.enable = true; # ensure ssh-agent is running
-  #services.ssh-agent.enable = false; # ensure ssh-agent is not running
+  #services.ssh-agent.enable = true; # ensure ssh-agent is running
+  services.ssh-agent.enable = false; # ensure ssh-agent is not running
   
   services.gpg-agent = {
     enable = true; # this setting adds export GPG_TTY lines to user's .zshrc and starts the agent on login
     enableScDaemon = true; # allow gpg-agent to use smartcards (e.g. yubikey)
-    enableSshSupport = false; # this setting adds 'gpg-connect-agent updatestartuptty /bye' to user's .zshrc to replace ssh-agent SSH_AUTH_SOCK with gpg-agent instead
-    #enableSshSupport = true; # this setting adds 'gpg-connect-agent updatestartuptty /bye' to user's .zshrc to replace ssh-agent SSH_AUTH_SOCK with gpg-agent instead
-    #sshKeys = [ # adds keygrip identifier to .gnupg/sshcontrol file and load gpg auth private key into gpg-agent
-    #  "DB9ADBBE6FBD1F0E694AF25D012321D46E090E61"
-    #];
-    #pinentryPackage = pkgs.pinentry-qt;
+    enableSshSupport = true; # this setting adds 'gpg-connect-agent updatestartuptty /bye' to user's .zshrc to replace ssh-agent SSH_AUTH_SOCK with gpg-agent instead
+    sshKeys = [ # adds keygrip identifier to .gnupg/sshcontrol file and load gpg auth private key into gpg-agent
+      "DB9ADBBE6FBD1F0E694AF25D012321D46E090E61"
+    ];
     pinentryPackage = pkgs.pinentry-rofi; # when enabling rofi in a compositor, requires rofi.nix module active
   };
-
-  #programs.zsh = {
-  #  profileExtra = # added to zsh login/global shell (.zprofile)
-  #  ''
-  #    GPG_TTY="$(tty)"
-  #    export GPG_TTY
-  #    /nix/store/94sh4d6iv54y6zwdqjzdrs77zj1pzsq1-gnupg-2.4.5/bin/gpg-connect-agent updatestartuptty /bye > /dev/null
-  #  '';
-  #};
 
 }
