8797d16cf7ade4c3501b29b7ba64939f29b98eee
dc-bond <chris@dcbond.com>
2024-09-15 08:05:33 -0400

    updates

--- home/chris/modules/firefox.nix
+++ home/chris/modules/firefox.nix
@@ -16,43 +16,58 @@
         name = "chris";
         path = "jribdwmn.default";
         isDefault = true;
-        #settings = {
-        #  "browser.startup.homepage" = "https://searx.aicampground.com";
-        #  "browser.search.defaultenginename" = "Searx";
-        #  "browser.search.order.1" = "Searx";
-        #};
-        #search = {
-        #  force = true;
-        #  default = "Searx";
-        #  order = [ "Searx" "Google" ];
-        #  engines = {
-        #    "Nix Packages" = {
-        #      urls = [{
-        #        template = "https://search.nixos.org/packages";
-        #        params = [
-        #          { name = "type"; value = "packages"; }
-        #          { name = "query"; value = "{searchTerms}"; }
-        #        ];
-        #      }];
-        #      icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-        #      definedAliases = [ "@np" ];
-        #    };
-        #    "NixOS Wiki" = {
-        #      urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
-        #      iconUpdateURL = "https://nixos.wiki/favicon.png";
-        #      updateInterval = 24 * 60 * 60 * 1000; # every day
-        #      definedAliases = [ "@nw" ];
-        #    };
-        #    "Searx" = {
-        #      urls = [{ template = "https://searx.aicampground.com/?q={searchTerms}"; }];
-        #      iconUpdateURL = "https://nixos.wiki/favicon.png";
-        #      updateInterval = 24 * 60 * 60 * 1000; # every day
-        #      definedAliases = [ "@searx" ];
-        #    };
-        #    "Bing".metaData.hidden = true;
-        #    "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
-        #  };
-        #};
+        settings = {
+          "browser.startup.homepage" = "https://search.opticon.dev";
+          "browser.search.defaultenginename" = "Opticon-SearXNG";
+          "browser.search.order.1" = "Opticon-SearXNG";
+        };
+        search = {
+          force = true;
+          default = "Opticon-SearXNG";
+          order = [ "Opticon-SearXNG" ];
+          engines = {
+            "Opticon-SearXNG" = {
+              urls = [{ template = "https://search.opticon.dev/?q={searchTerms}"; }];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@searx" ];
+            };
+            "Nix Packages" = {
+              urls = [{
+                template = "https://search.nixos.org/packages";
+                params = [
+                  { name = "type"; value = "packages"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+            "NixOS Wiki" = {
+              urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@nw" ];
+            };
+            #"Home Manager Options" = {
+						#	urls = [{	
+            #    template = "https://mipmip.github.io/home-manager-option-search";
+						#		params = [ 
+            #      { name = "query"; value = "{searchTerms}"; } 
+            #    ];
+						#	}];
+            #  iconUpdateURL = "https://nixos.wiki/favicon.png";
+						#	updateInterval = 24 * 60 * 60 *1000; # every day
+						#	definedAliases = [ "@hm" ];
+						#};
+          };
+        };
+        #bookmarks = [
+        #  {
+        #    name = "Bookmarks Toolbar"
+        #  }          
+        #];
+
       }; # default
 
     }; # profiles
@@ -225,10 +240,10 @@
         PreventInstalls = true;
         Add = [
           {
-            Name = "SearXNG";
+            Name = "Opticon-SearXNG";
             URLTemplate = "https://search.opticon.dev/?q={searchTerms}"; # accessible through vpn only
             Method = "GET"; # GET | POST
-            Description = "Self-Hosted SearXNG Instance";
+            Description = "Opticon Self-Hosted SearXNG Instance";
           }
         ];
         Remove = [
