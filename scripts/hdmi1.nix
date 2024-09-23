d2e62a50be57e09fcb19ebf6c5ec1e847f48f857
dc-bond <chris@dcbond.com>
2024-09-23 19:10:08 -0400

    updates

--- scripts/hdmi1.nix
+++
@@ -1,9 +0,0 @@
-{ 
-  pkgs, 
-  config,
-}:
-
-pkgs.writeShellScriptBin "hdmi1" 
-''
-  ddcutil -d 1 setvcp 60 0x11 
-''
--- scripts/hdmi2.nix
+++
@@ -1,9 +0,0 @@
-{ 
-  pkgs, 
-  config,
-}:
-
-pkgs.writeShellScriptBin "hdmi2" 
-''
-  ddcutil -d 1 setvcp 60 0x12 
-''
