7f7cae646ddfe380ecf764c63d1ed1b4cc3f1b46
dc-bond <chris@dcbond.com>
2024-10-01 16:01:02 -0400

    updates

--- scripts/host-specific/thinkpad/rebuild.nix
+++ scripts/host-specific/thinkpad/rebuild.nix
@@ -6,7 +6,7 @@
 pkgs.writeShellScriptBin "rebuild" 
 ''
   nixos_old_gen=$(readlink -f /run/current-system)
-  nixos-rebuild switch --flake ~/nixos-configs#thinkpad
+  sudo nixos-rebuild switch --flake ~/nixos-configs#thinkpad
   nixos_new_gen=$(readlink -f /run/current-system)
   nvd diff "$nixos_old_gen" "$nixos_new_gen"
 ''
