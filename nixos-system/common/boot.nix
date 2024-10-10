722c9e872e8db2f88eacc887d653890cea3c508f
dc-bond <chris@dcbond.com>
2024-10-10 11:52:42 -0400

    updates to boot

--- nixos-system/common/boot.nix
+++ nixos-system/common/boot.nix
@@ -13,11 +13,21 @@
       };
       efi.canTouchEfiVariables = true;
     };
+    supportedFilesystems = {
+      btrfs = true;
+      ext4 = true;
+    };
     kernel.sysctl = { "vm.swappiness" = 30;};
-    initrd.preLVMCommands = # turn on keyboard num-lock automatically during boot process
-    ''
-      ${pkgs.kbd}/bin/setleds +num
-    '';
+    initrd = {
+      supportedFilesystems = {
+        btrfs = true;
+        ext4 = true;
+      };
+      preLVMCommands = # turn on keyboard num-lock automatically during boot process
+        ''
+          ${pkgs.kbd}/bin/setleds +num
+        '';
+    };
   };
 
 }
