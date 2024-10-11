1e986bfe28acd7518d003c6c1870f6520398d98f
dc-bond <chris@dcbond.com>
2024-10-11 13:33:16 -0400

    updates

--- hosts/vm1/configuration.nix
+++ hosts/vm1/configuration.nix
@@ -13,7 +13,6 @@
   imports = lib.flatten [
     (map configLib.relativeToRoot [
       "hosts/vm1/disk-config-btrfs-luks.nix"
-      #"hosts/vm1/disk-config-btrfs.nix"
       "hosts/vm1/hardware-configuration.nix"
       "nixos-system/common/misc.nix"
       "nixos-system/common/boot.nix"
--- scripts/host-specific/thinkpad/vm1Rebuild.nix
+++ scripts/host-specific/thinkpad/vm1Rebuild.nix
@@ -5,13 +5,13 @@
 
 pkgs.writeShellScriptBin "vm1Rebuild" 
 ''
+  nixos_old_gen=$(ssh vm1 'readlink -f /run/current-system')
   nixos-rebuild \
   --flake ~/nixos-configs#vm1 \
   --target-host vm1 \
   --use-remote-sudo \
+  --verbose \
   switch
+  nixos_new_gen=$(ssh vm1 'readlink -f /run/current-system')
+  nvd diff "$nixos_old_gen" "$nixos_new_gen"
 ''
-  #nixos_old_gen=$(readlink -f /run/current-system)
-  #sudo nixos-rebuild switch --flake ~/nixos-configs#thinkpad
-  #nixos_new_gen=$(readlink -f /run/current-system)
-  #nvd diff "$nixos_old_gen" "$nixos_new_gen"
