6c7a10a4841ada9ae8ff86d268b2e850d3a643be
dc-bond <chris@dcbond.com>
2024-10-11 12:12:59 -0400

    updates

--- nixos-system/host-specific/thinkpad/packages.nix
+++ nixos-system/host-specific/thinkpad/packages.nix
@@ -8,6 +8,7 @@
 
   environment.systemPackages = with pkgs; [
     (import (configLib.relativeToRoot "scripts/host-specific/thinkpad/rebuild.nix") { inherit pkgs config; })
+    (import (configLib.relativeToRoot "scripts/host-specific/thinkpad/vm1Rebuild.nix") { inherit pkgs config; })
     brightnessctl # screen brightness application
     ddcutil # query and change monitor settings using DDC/CI and USB
     i2c-tools # hardware interface tools required by ddcutil
