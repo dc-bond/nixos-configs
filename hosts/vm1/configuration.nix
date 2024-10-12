9a937d4d35a02f4cf6240928ed939345caeb2a38
dc-bond <chris@dcbond.com>
2024-10-12 06:42:57 -0400

    updates

--- hosts/vm1/configuration.nix
+++ hosts/vm1/configuration.nix
@@ -12,7 +12,7 @@
 
   imports = lib.flatten [
     (map configLib.relativeToRoot [
-      "hosts/vm1/disk-config-btrfs-luks.nix"
+      "hosts/vm1/disk-config-btrfs.nix"
       "hosts/vm1/hardware-configuration.nix"
       "nixos-system/common/misc.nix"
       "nixos-system/common/boot.nix"
@@ -33,19 +33,13 @@
     (import (configLib.relativeToRoot "scripts/deployAspen.nix") { inherit pkgs config; })
     (import (configLib.relativeToRoot "scripts/getPassRepo.nix") { inherit pkgs config; })
     (import (configLib.relativeToRoot "scripts/rebuildLocalVm1.nix") { inherit pkgs config; })
-    age # encryption tool
-    sops # secrets management tool that can use different types of encryption (e.g. age, pgp, etc.)
     wget # download tool
     usbutils # package that provides 'lsusb' tool to see usb peripherals plugged in
     nvd # package version diff info for nix build operations
-    nix-tree # table view of package dependencies
-    ethtool # network tools
-    unzip # utility to unzip directories
     git # git
     eza # modern replacement for 'ls'
     pfetch # system info displayed on shell startup
     btop # system monitor
-    nmap # network scanning
   ];
 
 # original system state version - defines the first version of NixOS installed to maintain compatibility with application data (e.g. databases) created on older versions that can't automatically update their data when their package is updated
