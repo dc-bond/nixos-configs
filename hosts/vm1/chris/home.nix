095bb4f13044b588db6eb155b875d02752430b08
dc-bond <chris@dcbond.com>
2024-10-14 12:10:30 -0400

    updates

--- hosts/vm1/chris/home.nix
+++ hosts/vm1/chris/home.nix
@@ -19,7 +19,6 @@
       "home-manager/common/pass.nix"
       "home-manager/host-specific/vm1/chris/ssh.nix"
       "home-manager/host-specific/vm1/chris/aliases.nix"
-      #"home-manager/host-specific/vm1/chris/sops.nix"
     ])
   ];
 
