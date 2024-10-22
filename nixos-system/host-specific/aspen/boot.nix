f38274192ecda6cda6a91b21a50e23cb8fe54942
dc-bond <chris@dcbond.com>
2024-10-22 19:59:10 -0400

    updates

--- nixos-system/host-specific/aspen/boot.nix
+++ nixos-system/host-specific/aspen/boot.nix
@@ -7,15 +7,21 @@
 
   boot = {
     loader = {
-      grub = {
-        forceInstall = true;
-        #device = "nodev";
-        #extraConfig = ''
-        #  serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
-        #  terminal_input serial;
-        #  terminal_output serial
-        #'';
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 10; # only display last 10 generations
       };
+      efi.canTouchEfiVariables = true;
+      #grub = {
+      #  forceInstall = true;
+      #  device = "sda";
+      #  #device = "nodev";
+      #  #extraConfig = ''
+      #  #  serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+      #  #  terminal_input serial;
+      #  #  terminal_output serial
+      #  #'';
+      #};
       timeout = 10;
     };
     #kernelParams = [ "console=ttyS0,19200n8" ];
@@ -26,7 +32,7 @@
       supportedFilesystems = {
         ext4 = true;
       };
-       turn on keyboard num-lock automatically during boot process
+      # turn on keyboard num-lock automatically during boot process
       preLVMCommands = ''
         ${pkgs.kbd}/bin/setleds +num
       '';
