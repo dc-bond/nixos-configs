bc70c5abbf2a61f46bd819f38b04c4a638a06e2b
dc-bond <chris@dcbond.com>
2024-10-22 07:00:44 -0400

    updates

--- nixos-system/host-specific/aspen/boot.nix
+++ nixos-system/host-specific/aspen/boot.nix
@@ -9,16 +9,16 @@
     loader = {
       grub = {
         forceInstall = true;
-        device = "nodev";
-        extraConfig = ''
-          serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
-          terminal_input serial;
-          terminal_output serial
-        '';
+        #device = "nodev";
+        #extraConfig = ''
+        #  serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+        #  terminal_input serial;
+        #  terminal_output serial
+        #'';
       };
       timeout = 10;
     };
-    kernelParams = [ "console=ttyS0,19200n8" ];
+    #kernelParams = [ "console=ttyS0,19200n8" ];
     supportedFilesystems = {
       ext4 = true;
     };
@@ -26,10 +26,10 @@
       supportedFilesystems = {
         ext4 = true;
       };
-      # turn on keyboard num-lock automatically during boot process
-      #preLVMCommands = ''
-      #  ${pkgs.kbd}/bin/setleds +num
-      #'';
+       turn on keyboard num-lock automatically during boot process
+      preLVMCommands = ''
+        ${pkgs.kbd}/bin/setleds +num
+      '';
     };
   };
 
