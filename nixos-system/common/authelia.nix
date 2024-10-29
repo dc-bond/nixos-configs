df12324ea885758693c94e0fe09cc6c807fcefc5
dc-bond <chris@dcbond.com>
2024-10-29 15:43:34 -0400

    updates

--- nixos-system/common/authelia.nix
+++ nixos-system/common/authelia.nix
@@ -13,23 +13,23 @@ in
 
   sops.secrets = {
     autheliaJwtSecret = {
-      owner = config.users.users.three.name;
-      group = config.users.users.three.group;
+      owner = config.users.users."${app}-3".name;
+      group = config.users.users."${app}-3".group;
       mode = "0440";
     };
     autheliaStorageEncryptionKey = {
-      owner = config.users.users.three.name;
-      group = config.users.users.three.group;
+      owner = config.users.users."${app}-3".name;
+      group = config.users.users."${app}-3".group;
       mode = "0440";
     };
     autheliaSessionSecret = {
-      owner = config.users.users.three.name;
-      group = config.users.users.three.group;
+      owner = config.users.users."${app}-3".name;
+      group = config.users.users."${app}-3".group;
       mode = "0440";
     };
     autheliaOidcHmacSecret = {
-      owner = config.users.users.three.name;
-      group = config.users.users.three.group;
+      owner = config.users.users."${app}-3".name;
+      group = config.users.users."${app}-3".group;
       mode = "0440";
     };
     #autheliaOidcIssuerPrivateKey = {
@@ -37,94 +37,101 @@ in
     #};
   };
 
-  services.${app}.instances.three = {
-    enable = true; 
-    settings = {
-      #name = "${app}";
-      theme = "dark";
-      default_2fa_method = "webauthn";
-      default_redirection_url = "https://dcbond.com/";
-      log = {
-        level = "debug";
-        format = "text"; 
-        file_path = "/var/log/authelia/authelia.log";
-        keep_stdout = true;
+  services.${app}.instances = {
+    "3" = {
+      enable = true; 
+      settings = {
+        theme = "dark";
+        default_2fa_method = "webauthn";
+        default_redirection_url = "https://${configVars.domain1}/";
+        log = {
+          level = "debug";
+          format = "text"; 
+          file_path = "/var/lib/${app}-3/authelia.log";
+          keep_stdout = true;
+        };
+        server = {
+          host = "127.0.0.1";
+          port = 9091;
+        };
+        authentication_backend = {
+          file = {
+            path = "/var/lib/${app}-3/users-database.yml";
+          };
+        };
+        access_control = {
+          default_policy = "deny";
+          rules = [
+            #{
+            #  domain = ["${configVars.domain1}"];
+            #  resources = [
+            #    "^/wp-admin.*$"
+            #    "^/wp-admin/.*$"
+            #  ];
+            #  subject = "user:admin";
+            #  policy = "two_factor";
+            #}
+            #{
+            #  domain = ["excursion2025.${configVars.domain1}"];# only allow chris@dcbond.com user to authenticate to admin/login subfolders of excursion2025.dcbond.com (ghost admin page)
+            #  resources = [
+            #    "^/ghost.*$"
+            #    "^/ghost/.*$"
+            #  ];
+            #  subject = "user:admin";
+            #  policy = "two_factor";
+            #}
+            {
+              domain = [ # bypass authelia when connecting to authelia itself or when connecting to domain1
+                "identity.${configVars.domain3}"
+                #"${configVars.domain1}"
+              ];
+              policy = "bypass";
+            }
+          ];
+        };
+        session = {
+          name = "authelia_session";
+          domain = "${configVars.domain3}";
+          same_site = "lax";
+          expiration = "1h";
+          inactivity = "15m";
+          remember_me_duration = "24h";
+          redis.host = "/run/redis-${app}-3/redis.sock";
+        };
+        regulation = {
+          max_retries = 3;
+          find_time = "5m";
+          ban_time = "15m";
+        };
+        #storage = {
+        #  local = {
+        #    path = "/var/lib/authelia-main/db.sqlite3";
+        #  };
+        #};
+        notifier = {
+          disable_startup_check = false;
+          filesystem = {
+            filename = "/var/lib/${app}-3/notifications.txt";
+          };
+        };
       };
-      telemetry.metrics = {
-        enable = false;
-        address = "tcp://127.0.0.1:9959";
+      secrets = {
+        jwtSecretFile = "${config.sops.secrets.autheliaJwtSecret.path}";
+        storageEncryptionKeyFile = "${config.sops.secrets.autheliaStorageEncryptionKey.path}";
+        sessionSecretFile = "${config.sops.secrets.autheliaSessionSecret.path}";
+        oidcHmacSecretFile = "${config.sops.secrets.autheliaOidcHmacSecret.path}";
+        #oidcIssuerPrivateKeyFile = "${config.sops.secrets.autheliaOidcIssuerPrivateKey.path}";
       };
-      server.address = "tcp://:9091/";
-    };
-    secrets = {
-      jwtSecretFile = "${config.sops.secrets.autheliaJwtSecret.path}";
-      storageEncryptionKeyFile = "${config.sops.secrets.autheliaStorageEncryptionKey.path}";
-      sessionSecretFile = "${config.sops.secrets.autheliaSessionSecret.path}";
-      oidcHmacSecretFile = "${config.sops.secrets.autheliaOidcHmacSecret.path}";
-      #oidcIssuerPrivateKeyFile = "${config.sops.secrets.autheliaOidcIssuerPrivateKey.path}";
     };
   }; 
 
-      #server = {
-      #  host = "127.0.0.1";
-      #  port = 9091;
-      #};
-  
-      #authentication_backend = {
-      #  file = {
-      #    path = "/var/lib/authelia-main/users_database.yml";
-      #  };
-      #};
-  
-      #access_control = {
-      #  default_policy = "deny";
-      #  rules = [
-      #    {
-      #      domain = ["auth.example.com"];
-      #      policy = "bypass";
-      #    }
-      #    {
-      #      domain = ["*.example.com"];
-      #      policy = "one_factor";
-      #    }
-      #  ];
-      #};
-  
-      #session = {
-      #  name = "authelia_session";
-      #  expiration = "12h";
-      #  inactivity = "45m";
-      #  remember_me_duration = "1M";
-      #  domain = "example.com";
-      #  redis.host = "/run/redis-authelia-main/redis.sock";
-      #};
-  
-      #regulation = {
-      #  max_retries = 3;
-      #  find_time = "5m";
-      #  ban_time = "15m";
-      #};
-  
-      #storage = {
-      #  local = {
-      #    path = "/var/lib/authelia-main/db.sqlite3";
-      #  };
-      #};
-  
-      #notifier = {
-      #  disable_startup_check = false;
-      #  filesystem = {
-      #    filename = "/var/lib/authelia-main/notification.txt";
-      #  };
-      #};
-
-  #services.redis.servers.authelia-main = {
-  #  enable = true;
-  #  user = "authelia-main";   
-  #  port = 0;
-  #  unixSocket = "/run/redis-authelia-main/redis.sock";
-  #  unixSocketPerm = 600;
-  #};
+  services.redis.servers."${app}-3" = {
+    enable = true;
+    user = "${app}-3";   
+    port = 0;
+    unixSocket = "/run/redis-${app}-3/redis.sock";
+    unixSocketPerm = 600;
+  };
 
   services.traefik.dynamicConfigOptions.http = {
     routers.${app} = {
--- vars/default.nix
+++ vars/default.nix
@@ -7,6 +7,8 @@
 
   username = "chris";
   userEmail = "chris@dcbond.com";
+  domain1 = "dcbond.com";
+  domain2 = "opticon.dev";
   domain3 = "professorbond.com";
   #userFullName = inputs.nix-secrets.full-name;
   #userEmail = inputs.nix-secrets.userEmail;
