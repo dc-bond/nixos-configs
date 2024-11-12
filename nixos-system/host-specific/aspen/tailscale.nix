799522d6fa5aa1067e3c0e2108a83b0915fe695b
dc-bond <chris@dcbond.com>
2024-11-12 09:12:35 -0500

    updates

--- nixos-system/host-specific/aspen/tailscale.nix
+++ nixos-system/host-specific/aspen/tailscale.nix
@@ -10,11 +10,32 @@
     tailscaleAuthKey = {};
   };
 
+  networking.firewall = {
+    trustedInterfaces = [
+      "tailscale0"
+    ];
+  };
+
+  #systemd.network = {
+  #  networks = {
+  #    "20-tailscale" = {
+  #      matchConfig.Name = "tailscale0";
+  #      #networkConfig.DHCP = "ipv4";
+  #      #dhcpV4Config.RouteMetric = 300;
+  #      #dhcpV6Config.RouteMetric = 300;
+  #      linkConfig.RequiredForOnline = "no";
+  #    };    
+  #  };
+  #};
+
   services.tailscale = {
     enable = true;
     authKeyFile = "${config.sops.secrets.tailscaleAuthKey.path}";
     useRoutingFeatures = "client";
     #openFirewall = true;
+    #extraUpFlags = [
+    #  "--accept-dns=false" # disable magic DNS?
+    #];
   };
   
 }
--- vars/default.nix
+++ vars/default.nix
@@ -7,7 +7,7 @@
 
   userName = "chris";
   userEmail = "chris@dcbond.com";
-  aspenIp = "192.168.1.189";
+  aspenIp = "192.168.1.172";
   thinkpadIp = "192.168.1.62";
   opticonIp = "192.168.1.2";
   vm1Ip = "192.168.1.199";
