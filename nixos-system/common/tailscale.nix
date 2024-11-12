264c777995ade2adc94b5e89d95c878019f87d96
dc-bond <chris@dcbond.com>
2024-11-11 20:28:28 -0500

    updates

--- nixos-system/common/tailscale.nix
+++ nixos-system/common/tailscale.nix
@@ -1,12 +1,18 @@
 { 
   pkgs, 
+  config,
   ... 
 }: 
 
 {
 
+  sops.secrets = {
+    tailscaleAuthKey = {};
+  };
+
   services.tailscale = {
     enable = true;
+    authKeyFile = "${config.sops.secrets.tailscaleAuthKey.path}";
     openFirewall = true;
   };
   
