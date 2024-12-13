acde462ace5c97e5bcd7eaaa86f8f927f691c4aa
dc-bond <chris@dcbond.com>
2024-12-13 17:25:55 -0500

    updates

--- nixos-system/common/home-assistant.nix
+++ nixos-system/common/home-assistant.nix
@@ -13,11 +13,11 @@
 
     home-assistant = {
       enable = true;
-      #package = (pkgs.home-assistant.override {
-      #  extraPackages = py: with py; [ psycopg2 ];
-      #}).overrideAttrs (oldAttrs: {
-      #  doInstallCheck = false;
-      #});
+      package = (pkgs.home-assistant.override {
+        extraPackages = py: with py; [ psycopg2 ];
+        }).overrideAttrs (oldAttrs: {
+          doInstallCheck = false;
+        });
       extraComponents = [
         "default_config"
         "esphome"
@@ -26,36 +26,30 @@
       ];
       config = {
         http.server_port = 8123;
-        #recorder.db_url = "postgresql://@/homeassistant";
+        recorder.db_url = "postgresql://@/homeassistant";
         #frontend = {
         #  themes = "!include_dir_merge_named themes";
-        #};
-        #homeassistant = {
-        #  name = "Fort Hemingway";
-        #  temperature_unit = "F";
-        #  time_zone = "America/New_York";
-        #  unit_system = "metric";
         #};
       };
     };
 
-    #postgresql = {
-    #  enable = true;
-    #  ensureDatabases = [ "homeassistant" ];
-    #  ensureUsers = [
-    #    {
-    #      name = "homeassistant";
-    #      ensureDBOwnership = true;
-    #      #ensureClauses.createdb = true;
-    #    }
-    #  ];
-    #};
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "homeassistant" ];
+      ensureUsers = [
+        {
+          name = "homeassistant";
+          ensureDBOwnership = true;
+          #ensureClauses.createdb = true;
+        }
+      ];
+    };
 
-    #postgresqlBackup = { # postgres database backup
-    #  enable = true;
-    #  databases = [ "homeassistant" ];
-    #  startAt = "*-*-* 01:00:00"; # daily starting at 1:00am
-    #};
+    postgresqlBackup = { # postgres database backup
+      enable = true;
+      databases = [ "homeassistant" ];
+      startAt = "*-*-* 01:00:00"; # daily starting at 1:00am
+    };
 
   };
 
