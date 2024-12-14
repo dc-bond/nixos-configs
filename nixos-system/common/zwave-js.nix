c7c0052add926e12632a168cd43d716217c4277a
dc-bond <chris@dcbond.com>
2024-12-13 19:04:36 -0500

    updates

--- nixos-system/common/zwave-js.nix
+++ nixos-system/common/zwave-js.nix
@@ -2,17 +2,33 @@
   config, 
   pkgs, 
   configVars,
+  configLib,
   ... 
 }: 
 
 {
+
+  sops.secrets = {
+    zwaveSecurityKeys = {
+      sopsFile = configLib.relativeToRoot "hosts/cypress/zwave-security-keys.json";
+      format = "json";
+    };
+  };
+
+  sops.secrets.github_token = {
+    # The sops file can be also overwritten per secret...
+    sopsFile = ./other-secrets.json;
+    # ... as well as the format
+    format = "json";
+  };
 
   services = {
 
     zwave-js = {
       enable = true;
       port = 3000;
-      #serialPort = "/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_d677b47b5594eb11ba3436703d98b6d1-if00-port0:/dev/zwave";
+      serialPort = "/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_d677b47b5594eb11ba3436703d98b6d1-if00-port0:/dev/zwave";
+      secretsConfigFile = "${config.sops.secrets.zwaveSecurityKeys.path}";
       #settings = {
       #};
     };
