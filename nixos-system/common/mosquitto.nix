3994dc636021b591b0d22bac8a49c5710deee766
dc-bond <chris@dcbond.com>
2024-12-18 15:09:21 -0500

    updates

--- nixos-system/common/mosquitto.nix
+++ nixos-system/common/mosquitto.nix
@@ -7,11 +7,24 @@
 
 {
 
+  #networking.firewall.allowedTCPPorts = [ 1883 ];
+
+  sops.secrets.mqttHassPasswd = {};
+
   services = {
 
     mosquitto = {
       enable = true;
       logType = [ "error" ];
+      logDest = [ "syslog" ];
+      listeners = [
+        {
+          users.hass = {
+            acl = [ "readwrite #" ];
+            passwordFile = "${config.sops.secrets.mqttHassPasswd.path}";
+          };
+        }
+      ];
     };
 
   };
--- nixos-system/common/oci-containers.nix
+++ nixos-system/common/oci-containers.nix
@@ -16,13 +16,13 @@
         enable = true; # run rootless
         setSocketVariable = true; # set DOCKER_HOST variable to the rootless docker instance for normal users by default
       };
-      daemon.settings = {
-        userland-proxy = false;
-        experimental = true;
-        metrics-addr = "0.0.0.0:9323";
-        ipv6 = false;
-        #fixed-cidr-v6 = "fd00::/80";
-      };
+      #daemon.settings = {
+      #  userland-proxy = false;
+      #  experimental = true;
+      #  metrics-addr = "0.0.0.0:9323";
+      #  ipv6 = false;
+      #  #fixed-cidr-v6 = "fd00::/80";
+      #};
     };
   };
 
