db5310195916b66cb6afbb5e303035c88508110a
dc-bond <chris@dcbond.com>
2024-12-20 19:30:21 -0500

    updates

--- home-manager/host-specific/thinkpad/chris/aliases.nix
+++ home-manager/host-specific/thinkpad/chris/aliases.nix
@@ -11,7 +11,7 @@
       rbcypress = "rebuild-remote-cypress";
       rbaspen = "rebuild-remote-aspen";
       getnets = "iwctl station wlan0 get-networks";
-      flakeupdate = "sudo nix flake update --flake --verbose ~/nixos-configs";
+      flakeupdate = "sudo nix flake update --flake ~/nixos-configs";
       borglistcypress = "sudo borg list /var/lib/borg-backups/cypress";
       borginfocypress = "sudo borg info /var/lib/borg-backups/cypress";
     };
