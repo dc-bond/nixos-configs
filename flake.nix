1b2489f4a8c7fc52706eaad52e6d918436fb225c
dc-bond <chris@dcbond.com>
2024-12-31 09:11:22 -0500

    updates

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    impermanence.url = "github:nix-community/impermanence";
+    #impermanence.url = "github:nix-community/impermanence";
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
