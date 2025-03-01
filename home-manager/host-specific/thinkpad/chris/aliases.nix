4805e9f2f3e83f866b6ec1a951ca33d25811c993
dc-bond <chris@dcbond.com>
2025-03-01 07:01:12 -0500

    .

--- home-manager/host-specific/thinkpad/chris/aliases.nix
+++ home-manager/host-specific/thinkpad/chris/aliases.nix
@@ -8,9 +8,9 @@
 
   programs.zsh = {
     shellAliases = {
-      rbthink = "rebuildLocalThinkpad";
-      rbcyp = "rebuildRemoteCypress";
-      rbasp = "rebuildRemoteAspen";
+      rb = "rebuildLocalThinkpad";
+      rbcypress = "rebuildRemoteCypress";
+      rbaspen = "rebuildRemoteAspen";
       getnets = "iwctl station wlan0 get-networks";
       flakeupdate = "sudo nix flake update --flake ~/nixos-configs";
     };
