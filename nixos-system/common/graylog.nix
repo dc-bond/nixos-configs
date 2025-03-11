6de884a692babc6ad2d22dee1ebbd621a86b79d1
dc-bond <chris@dcbond.com>
2025-03-11 12:27:13 -0400

    .

--- nixos-system/common/graylog.nix
+++ nixos-system/common/graylog.nix
@@ -20,9 +20,12 @@ in
       enable = true;
       passwordSecret = "3LgC0W6UenLkPdPGhT94rO7V7Sar6RN2mN7DhNpi2kQHhON1TsA5QhZxdTHTijkdeMoPQTAtYElyxL8GlZkXVrb2dv2tNKEF";
       rootPasswordSha2 = "0ea98db27a78fd15d23172feb03e583ba9b055d21520ed6607eb331ac92bc570";
+      rootUsername = "${configVars.userEmail}";
       extraConfig = ''
         http_external_uri = https://${app}.${configVars.domain2}/
         java.net.preferIPv4Stack = true
+        root_timezone = America/New_York
+        root_email = ${configVars.userEmail}
       '';
       elasticsearchHosts = [ "http://127.0.0.1:9200" ];
     };
