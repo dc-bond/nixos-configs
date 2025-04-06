354b3310a11160b1cfe9ad1f419309671a8f850d
dc-bond <chris@dcbond.com>
2025-04-06 15:15:30 -0400

    .

--- nixos-system/common/cloud-backups.nix
+++ nixos-system/common/cloud-backups.nix
@@ -20,7 +20,7 @@ let
     #!/bin/bash
 
     # define available hosts
-    HOSTS=("aspen", "cypress" "thinkpad")
+    HOSTS=("aspen" "cypress" "thinkpad")
     
     # display menu options for hosts
     echo "Select a host to recover:"
@@ -56,7 +56,7 @@ let
     #!/bin/bash
 
     # define available hosts
-    HOSTS=("aspen", "cypress" "thinkpad")
+    HOSTS=("aspen" "cypress" "thinkpad")
     
     # display menu options for hosts
     echo "Select a host:"
