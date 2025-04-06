09ce3b0d090c119b30fcab087a2f751d2f522366
dc-bond <chris@dcbond.com>
2025-04-06 15:16:03 -0400

    .

--- hosts/aspen/configuration.nix
+++ hosts/aspen/configuration.nix
@@ -95,7 +95,6 @@
 
       "nixos-system/host-specific/aspen/oci-media-server.nix"
       "nixos-system/host-specific/aspen/oci-frigate.nix" # requires nvidia.nix
-      #"nixos-system/host-specific/aspen/frigate.nix" # requires mosquitto.nix, nvidia.nix
       "nixos-system/host-specific/aspen/nvidia.nix"
       "nixos-system/host-specific/aspen/borg-backups.nix"
       "nixos-system/host-specific/aspen/networking.nix"
