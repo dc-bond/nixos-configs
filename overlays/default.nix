42d772c1a262be72a452e9c6eb175617471e3e76
dc-bond <chris@dcbond.com>
2025-04-27 08:38:52 -0400

    .

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,4 +13,12 @@
     };
   };
 
+  #kasmweb = final: _prev: {
+  #  src = final.fetchurl {
+  #    url = "https://kasm-static-content.s3.amazonaws.com/kasm_release_1.17.0.bbc15c.tar.gz";
+  #    sha256 = "1zl64x59a38jwdxd2lf88hpj0ii1jkwbb0rpa4znpgmvq1686a5j";
+  #  };
+  #  version = "1.17.0";
+  #};
+
 }
