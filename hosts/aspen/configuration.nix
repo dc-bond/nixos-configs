c1c62fb4940e88512d5d509a8510503861d64cb8
dc-bond <chris@dcbond.com>
2025-05-24 07:18:33 -0400

    updates

--- hosts/aspen/configuration.nix
+++ hosts/aspen/configuration.nix
@@ -27,35 +27,45 @@
     };
 
     environment.systemPackages = with pkgs; [
-      (import (configLib.relativeToRoot "scripts/get-pass-repo.nix") { inherit pkgs config; })
-      age # encryption tool
-      mkpasswd # password hashing tool
-      dig # dns lookup tool
-      sops # secrets management tool that can use different types of encryption (e.g. age, pgp, etc.)
       wget # download tool
-      rsync # sync tool
       usbutils # package that provides 'lsusb' tool to see usb peripherals plugged in
       nvd # package version diff info for nix build operations
-      nix-tree # table view of package dependencies
-      ethtool # network tools
-      inetutils # more network tools like telnet
-      unzip # utility to unzip directories
+      rsync # sync tool
       git # git
+      dig # dns lookup tool
       eza # modern replacement for 'ls'
       pfetch # system info displayed on shell startup
       btop # system monitor
       nmap # network scanning
-      ddcutil # query and change monitor settings using DDC/CI and USB
-      i2c-tools # hardware interface tools required by ddcutil
-      libreoffice-still # office suite
-      element-desktop # matrix chat app
-      hollywood # fill terminal with melodramatic technobabble
-      cool-retro-term # retro terminal
-      imagemagick # photo tool
-      ffmpeg-full # video tool
+      #(import (configLib.relativeToRoot "scripts/get-pass-repo.nix") { inherit pkgs config; })
+      #age # encryption tool
+      #mkpasswd # password hashing tool
+      #dig # dns lookup tool
+      #sops # secrets management tool that can use different types of encryption (e.g. age, pgp, etc.)
+      #wget # download tool
+      #rsync # sync tool
+      #usbutils # package that provides 'lsusb' tool to see usb peripherals plugged in
+      #nvd # package version diff info for nix build operations
+      #nix-tree # table view of package dependencies
+      #ethtool # network tools
+      #inetutils # more network tools like telnet
+      #unzip # utility to unzip directories
+      #git # git
+      #eza # modern replacement for 'ls'
+      #pfetch # system info displayed on shell startup
+      #btop # system monitor
+      #nmap # network scanning
+      #ddcutil # query and change monitor settings using DDC/CI and USB
+      #i2c-tools # hardware interface tools required by ddcutil
+      #libreoffice-still # office suite
+      #element-desktop # matrix chat app
+      #hollywood # fill terminal with melodramatic technobabble
+      #cool-retro-term # retro terminal
+      #imagemagick # photo tool
+      #ffmpeg-full # video tool
     ];
 
-    hardware.i2c.enable = true; # enable i2c kernel module for ddcutil functionality
+    #hardware.i2c.enable = true; # enable i2c kernel module for ddcutil functionality
     
     # original system state version - defines the first version of NixOS installed to maintain compatibility with application data (e.g. databases) created on older versions that can't automatically update their data when their package is updated
     system.stateVersion = "24.11";
@@ -64,46 +74,66 @@
 
   imports = lib.flatten [
     (map configLib.relativeToRoot [
+      
       "hosts/aspen/disk-config-btrfs.nix"
       "hosts/aspen/hardware-configuration.nix"
-      "nixos-system/common/audio.nix"
+      #"nixos-system/common/audio.nix"
       "nixos-system/common/boot.nix"
       "nixos-system/common/zsh.nix"
-      "nixos-system/common/yubikey.nix"
+      #"nixos-system/common/yubikey.nix"
       "nixos-system/common/misc.nix"
       "nixos-system/common/nixpkgs.nix"
       "nixos-system/common/cloud-backups.nix"
       "nixos-system/common/sops.nix"
-      "nixos-system/common/bluetooth.nix"
+      #"nixos-system/common/bluetooth.nix"
+      "nixos-system/common/nvidia.nix"
 
-      "nixos-system/common/keyring.nix"
-      "nixos-system/common/login.nix"
-      "nixos-system/common/thunar.nix"
-      "nixos-system/common/hyprland.nix"
-      "nixos-system/common/printing.nix"
-      "nixos-system/common/fonts.nix"
-      
-      "nixos-system/common/traefik.nix"
-      #"nixos-system/common/postgresql.nix"
-      "nixos-system/common/mysql.nix"
-      "nixos-system/common/photoprism.nix" # requires mysql.nix
-      #"nixos-system/common/mosquitto.nix"
-      #"nixos-system/common/graylog.nix"
-      "nixos-system/common/kasmweb.nix"
-      #"nixos-system/common/writefreely.nix" # requires mysql.nix
-      "nixos-system/common/oci-containers.nix"
-      "nixos-system/common/oci-fava.nix"
+      #"nixos-system/common/keyring.nix"
+      #"nixos-system/common/login.nix"
+      #"nixos-system/common/thunar.nix"
+      #"nixos-system/common/hyprland.nix"
+      #"nixos-system/common/printing.nix"
+      #"nixos-system/common/fonts.nix"
 
-      "nixos-system/host-specific/aspen/oci-media-server.nix"
-      "nixos-system/host-specific/aspen/oci-frigate.nix" # requires nvidia.nix
-      "nixos-system/host-specific/aspen/nvidia.nix"
       "nixos-system/host-specific/aspen/borg-backups.nix"
       "nixos-system/host-specific/aspen/networking.nix"
       "nixos-system/host-specific/aspen/sshd.nix"
       "nixos-system/host-specific/aspen/tailscale.nix"
       "nixos-system/host-specific/aspen/users.nix"
       #"nixos-system/host-specific/aspen/journal2gelf.nix"
-      #"nixos-system/host-specific/aspen/gaming.nix"
+      
+      "nixos-system/common/traefik.nix"
+      "nixos-system/common/postgresql.nix"
+      "nixos-system/common/mysql.nix"
+      "nixos-system/common/photoprism.nix" # requires mysql.nix
+      #"nixos-system/common/lldap.nix" # requires postgresql.nix
+      #"nixos-system/common/mosquitto.nix"
+      #"nixos-system/common/uptime-kuma.nix"
+      #"nixos-system/common/graylog.nix"
+      #"nixos-system/common/kasmweb.nix"
+      #"nixos-system/common/writefreely.nix" # requires mysql.nix
+      #"nixos-system/common/nextcloud.nix" # requires postgresql.nix
+      #"nixos-system/common/home-assistant.nix" # requires postgresql.nix, mosquitto.nix
+      #"nixos-system/common/authelia-dcbond.nix" # requires lldap.nix
+      #"nixos-system/common/stirling-pdf.nix"
+      #"nixos-system/common/roundcube.nix" # requires postgresl.nix
+      #"nixos-system/common/matrix.nix" # requires postgresql.nix
+      #"nixos-system/common/privatebin.nix"
+      #"nixos-system/common/dcbond-root.nix"
+      #"nixos-system/common/unifi-controller.nix" # compile problems with mongodb
+      "nixos-system/common/oci-containers.nix"
+      "nixos-system/common/oci-fava.nix"
+      "nixos-system/common/oci-media-server.nix" # requires nvidia.nix
+      "nixos-system/common/oci-frigate.nix" # requires nvidia.nix
+      #"nixos-system/common/oci-unifi-controller.nix"
+      #"nixos-system/common/oci-pihole.nix"
+      #"nixos-system/common/oci-actual.nix"
+      #"nixos-system/common/oci-zwavejs.nix"
+      #"nixos-system/common/oci-chromium.nix"
+      #"nixos-system/common/oci-searxng.nix"
+      #"nixos-system/common/oci-wordpress-dcbond.nix"
+      #"nixos-system/common/oci-recipesage.nix"
+      #"nixos-system/common/oci-librechat.nix"
 
       "scripts/rebuild/rebuild-local-aspen.nix"
       "scripts/rebuild/rebuild-remote-thinkpad.nix"
@@ -112,7 +142,6 @@
       "scripts/deploy/deploy-cypress.nix"
       "scripts/photo-renumber.nix"
       "scripts/media-transfer.nix"
-      
       "scripts/backup-recovery/recover-family-media.nix"
       #"scripts/backup-recovery/recover-email.nix"
       #"scripts/backup-recovery/recover-traefik.nix"
--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -10,6 +10,34 @@
 
 {
 
+  environment.systemPackages = with pkgs; [
+    (import (configLib.relativeToRoot "scripts/get-pass-repo.nix") { inherit pkgs config; })
+    age # encryption tool
+    mkpasswd # password hashing tool
+    dig # dns lookup tool
+    sops # secrets management tool that can use different types of encryption (e.g. age, pgp, etc.)
+    wget # download tool
+    rsync # sync tool
+    usbutils # package that provides 'lsusb' tool to see usb peripherals plugged in
+    nvd # package version diff info for nix build operations
+    nix-tree # table view of package dependencies
+    ethtool # network tools
+    inetutils # more network tools like telnet
+    unzip # utility to unzip directories
+    git # git
+    eza # modern replacement for 'ls'
+    pfetch # system info displayed on shell startup
+    btop # system monitor
+    nmap # network scanning
+    brightnessctl # screen brightness application
+    ddcutil # query and change monitor settings using DDC/CI and USB
+    i2c-tools # hardware interface tools required by ddcutil
+    libreoffice-still # office suite
+    element-desktop # matrix chat app
+    hollywood # fill terminal with melodramatic technobabble
+    cool-retro-term # retro terminal
+  ];
+
   imports = lib.flatten [
     (map configLib.relativeToRoot [
       "hosts/thinkpad/disk-config-btrfs-luks.nix"
@@ -54,36 +82,6 @@
       "scripts/deploy/deploy-aspen.nix"
       "scripts/deploy/deploy-cypress.nix"
     ])
-  ];
-
-  environment.systemPackages = with pkgs; [
-    (import (configLib.relativeToRoot "scripts/get-pass-repo.nix") { inherit pkgs config; })
-    age # encryption tool
-    mkpasswd # password hashing tool
-    dig # dns lookup tool
-    sops # secrets management tool that can use different types of encryption (e.g. age, pgp, etc.)
-    wget # download tool
-    rsync # sync tool
-    usbutils # package that provides 'lsusb' tool to see usb peripherals plugged in
-    nvd # package version diff info for nix build operations
-    nix-tree # table view of package dependencies
-    ethtool # network tools
-    inetutils # more network tools like telnet
-    unzip # utility to unzip directories
-    git # git
-    eza # modern replacement for 'ls'
-    pfetch # system info displayed on shell startup
-    btop # system monitor
-    nmap # network scanning
-    brightnessctl # screen brightness application
-    ddcutil # query and change monitor settings using DDC/CI and USB
-    i2c-tools # hardware interface tools required by ddcutil
-    libreoffice-still # office suite
-    element-desktop # matrix chat app
-    hollywood # fill terminal with melodramatic technobabble
-    cool-retro-term # retro terminal
-    filelight # disk usage visualizer
-    yai # terminal ai utility
   ];
 
   hardware.i2c.enable = true; # enable i2c kernel module for ddcutil functionality
