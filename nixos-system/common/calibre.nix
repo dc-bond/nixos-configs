708c3bf403d47011e61e38571544f8863926d6fe
dc-bond <chris@dcbond.com>
2025-06-25 11:12:09 -0400

    .

--- nixos-system/common/calibre.nix
+++ nixos-system/common/calibre.nix
@@ -7,7 +7,7 @@
 }: 
 
 let
-  app = "calibre-server";
+  #app = "calibre-server";
   app2 = "calibre-web";
 in
 
@@ -15,15 +15,16 @@ in
 
   services = {
 
-    "${app}" = {
-      enable = true;
-      port = 7189;
-      libraries = [ "${config.drives.storageDrive1}/media/ebooks/calibre/" ];
-    };
+    #"${app}" = {
+    #  enable = true;
+    #  port = 7189;
+    #  libraries = [ "${config.drives.storageDrive1}/media/ebooks/calibre/" ];
+    #};
 
     "${app2}" = {
       enable = true;
-      user = "calibre-server";
+      user = "calibre-web";
+      group = "calibre-web";
       listen = {
        ip = "127.0.0.1";
        port = 7190;
@@ -37,18 +38,18 @@ in
 
     traefik.dynamicConfigOptions.http = {
       routers = {
-        ${app} = {
-          entrypoints = ["websecure"];
-          rule = "Host(`${app}.${configVars.domain2}`)";
-          service = "${app}";
-          middlewares = [
-            "secure-headers"
-          ];
-          tls = {
-            certResolver = "cloudflareDns";
-            options = "tls-13@file";
-          };
-        };
+        #${app} = {
+        #  entrypoints = ["websecure"];
+        #  rule = "Host(`${app}.${configVars.domain2}`)";
+        #  service = "${app}";
+        #  middlewares = [
+        #    "secure-headers"
+        #  ];
+        #  tls = {
+        #    certResolver = "cloudflareDns";
+        #    options = "tls-13@file";
+        #  };
+        #};
         ${app2} = {
           entrypoints = ["websecure"];
           rule = "Host(`${app2}.${configVars.domain2}`)";
@@ -63,16 +64,16 @@ in
         };
       };
       services = {
-        ${app} = {
-          loadBalancer = {
-            passHostHeader = true;
-            servers = [
-            {
-              url = "http://127.0.0.1:7189";
-            }
-            ];
-          };
-        };
+        #${app} = {
+        #  loadBalancer = {
+        #    passHostHeader = true;
+        #    servers = [
+        #    {
+        #      url = "http://127.0.0.1:7189";
+        #    }
+        #    ];
+        #  };
+        #};
         ${app2} = {
           loadBalancer = {
             passHostHeader = true;
