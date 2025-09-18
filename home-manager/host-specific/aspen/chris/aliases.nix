2828254a55787a6c3b0e465a00b418029bef0f47
dc-bond <chris@dcbond.com>
2025-09-18 12:28:40 -0400

    .

--- home-manager/host-specific/aspen/chris/aliases.nix
+++ home-manager/host-specific/aspen/chris/aliases.nix
@@ -8,6 +8,7 @@
 
   programs.zsh = {
     shellAliases = {
+      rb = "sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs#aspen";
       storage = "cd /storage/WD-WCC7K4RU947F ; ls";
     };
   };
--- home-manager/host-specific/juniper/chris/aliases.nix
+++ home-manager/host-specific/juniper/chris/aliases.nix
@@ -8,6 +8,7 @@
 
   programs.zsh = {
     shellAliases = {
+      rb = "sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs#juniper";
     };
   };
 
