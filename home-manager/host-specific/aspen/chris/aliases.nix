150d777a923107c719555fa7b8782353d5b099fd
dc-bond <chris@dcbond.com>
2025-09-18 12:39:28 -0400

    .

--- home-manager/host-specific/aspen/chris/aliases.nix
+++ home-manager/host-specific/aspen/chris/aliases.nix
@@ -8,7 +8,7 @@
 
   programs.zsh = {
     shellAliases = {
-      rb = "sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs#aspen";
+      rb = "nix flake update github:dc-bond/nixos-configs &&sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs#aspen";
       storage = "cd /storage/WD-WCC7K4RU947F ; ls";
     };
   };
