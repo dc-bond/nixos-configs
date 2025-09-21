6aea1fe42ee5935c935eef28b91b9eaea963134c
dc-bond <chris@dcbond.com>
2025-09-21 09:19:50 -0400

    .

--- scripts/get-pass-repo.nix
+++ scripts/get-pass-repo.nix
@@ -1,16 +1,27 @@
 { 
   pkgs,
+  ...
 }:
 
-pkgs.writeShellScriptBin "getPassRepo" ''
-  cd ~
-  git clone git@github.com:dc-bond/.password-store.git
-  touch ~/.password-store/.git/hooks/post-commit
-  cat >> ~/.password-store/.git/hooks/post-commit << 'END'
-  #!/bin/sh
-  set -x
-  git pull --rebase # get edits by other hosts
-  git push # push the latest commit
-  END
-  chmod +x ~/.password-store/.git/hooks/post-commit
-''
+let
+
+  getPassRepoScript = pkgs.writeShellScriptBin "getPassRepo" ''
+    cd ~
+    git clone git@github.com:dc-bond/.password-store.git
+    touch ~/.password-store/.git/hooks/post-commit
+    cat >> ~/.password-store/.git/hooks/post-commit << 'END'
+    #!/bin/sh
+    set -x
+    git pull --rebase # get edits by other hosts
+    git push # push the latest commit
+    END
+    chmod +x ~/.password-store/.git/hooks/post-commit
+  '';
+
+in
+
+{
+
+  environment.systemPackages = with pkgs; [ getPassRepoScript ];
+
+}  
