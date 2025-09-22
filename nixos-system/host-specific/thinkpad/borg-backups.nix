38b0cdd5017cfb41da010bfb2e6b6049c6261ca1
dc-bond <chris@dcbond.com>
2025-09-22 09:40:52 -0400

    .

--- nixos-system/host-specific/thinkpad/borg-backups.nix
+++ nixos-system/host-specific/thinkpad/borg-backups.nix
@@ -3,6 +3,7 @@
   config,
   lib,
   configVars,
+  configLib,
   ... 
 }: 
 
@@ -50,6 +51,40 @@ in
       infoLocalArchivesScript
     ];
 
+    services.borgbackup.jobs = {
+      "${config.networking.hostName}" = {
+        archiveBaseName = "${config.networking.hostName}";
+        repo = "${config.backups.borgDir}/${config.networking.hostName}";
+        dateFormat = "+%Y.%m.%d-T%H:%M:%S";
+        doInit = true; # run borg init if backup directory does not already contain the repository
+        failOnWarnings = false;
+        extraCreateArgs = [
+          "--progress"
+          "--stats"
+        ];
+        startAt = "*-*-* 00:10:00"; # everyday at 12:10am
+        encryption = {
+          mode = "repokey-blake2"; # encrypt using password and save encryption key inside repository
+          passCommand = "cat ${config.sops.secrets.borgCryptPasswd.path}";
+        };
+        environment = { 
+          BORG_RELOCATED_REPO_ACCESS_IS_OK = "yes"; # supress warning about repo location being moved since last backup (e.g. changing directory location or IP address)
+        };
+        compression = "auto,zstd,8";
+        postHook = ''
+          echo "starting cloud backup"
+          systemctl start cloudBackup.service
+        '';
+        paths = [
+          "/home/${configVars.userName}/email"
+        ];
+        prune.keep = {
+          daily = 7; # keep the last seven daily archives
+          monthly = 6; # keep the last six monthly archives
+        };
+      };
+    };
+    
   };
 
 }
