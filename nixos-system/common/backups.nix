3585d8ca2e417c4f77093059d91a7f08079a6b5d
dc-bond <chris@dcbond.com>
2025-09-28 07:13:19 -0400

    .

--- nixos-system/common/backups.nix
+++ nixos-system/common/backups.nix
@@ -16,10 +16,6 @@ let
     #!/bin/bash
     export BORG_PASSPHRASE=$(sudo cat ${borgCryptPasswdFile})
     export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
-    export BORG_CACHE_DIR="${config.backups.borgDir}/.cache"
-    export BORG_SECURITY_DIR="${config.backups.borgDir}/.config/security"
-    export BORG_KEYS_DIR="${config.backups.borgDir}/.config/keys"
-    export TMPDIR="${config.backups.borgDir}/.tmp"
     sudo -E ${pkgs.borgbackup}/bin/borg list --short ${config.backups.borgDir}/${config.networking.hostName}
   '';
 
@@ -27,10 +23,6 @@ let
     #!/bin/bash
     export BORG_PASSPHRASE=$(sudo cat ${borgCryptPasswdFile})
     export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
-    export BORG_CACHE_DIR="${config.backups.borgDir}/.cache"
-    export BORG_SECURITY_DIR="${config.backups.borgDir}/.config/security"
-    export BORG_KEYS_DIR="${config.backups.borgDir}/.config/keys"
-    export TMPDIR="${config.backups.borgDir}/.tmp"
     sudo -E ${pkgs.borgbackup}/bin/borg info ${config.backups.borgDir}/${config.networking.hostName}
   '';
   
@@ -61,10 +53,6 @@ let
     export BORG_PASSPHRASE=$(sudo cat ${borgCryptPasswdFile})
     export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
     export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
-    export BORG_CACHE_DIR="${config.backups.borgCloudDir}/.cache"
-    export BORG_SECURITY_DIR="${config.backups.borgCloudDir}/.config/security"
-    export BORG_KEYS_DIR="${config.backups.borgCloudDir}/.config/keys"
-    export TMPDIR="${config.backups.borgCloudDir}/.tmp"
     sudo -E ${pkgs.borgbackup}/bin/borg list --short ${config.backups.borgCloudDir}/${config.networking.hostName}
   '';
 
@@ -73,10 +61,6 @@ let
     export BORG_PASSPHRASE=$(sudo cat ${borgCryptPasswdFile})
     export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
     export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
-    export BORG_CACHE_DIR="${config.backups.borgCloudDir}/.cache"
-    export BORG_SECURITY_DIR="${config.backups.borgCloudDir}/.config/security"
-    export BORG_KEYS_DIR="${config.backups.borgCloudDir}/.config/keys"
-    export TMPDIR="${config.backups.borgCloudDir}/.tmp"
     sudo -E ${pkgs.borgbackup}/bin/borg info ${config.backups.borgCloudDir}/${config.networking.hostName}
   '';
 
@@ -157,10 +141,6 @@ in
         };
         environment = { 
           BORG_RELOCATED_REPO_ACCESS_IS_OK = "yes"; # supress warning about repo location being moved since last backup (e.g. changing directory location or IP address)
-          BORG_CACHE_DIR = "${config.backups.borgDir}/.cache"; 
-          BORG_SECURITY_DIR = "${config.backups.borgDir}/.config/security";
-          BORG_KEYS_DIR = "${config.backups.borgDir}/.config/keys";
-          TMPDIR = "${config.backups.borgDir}/.tmp";
         };
         compression = "auto,zstd,8";
         preHook = lib.mkDefault ''
@@ -184,23 +164,7 @@ in
     };
 
     systemd = {
-      tmpfiles.rules = [
-        "d ${config.backups.borgDir}/.cache 0700 root root -"
-        "d ${config.backups.borgDir}/.config 0700 root root -"
-        "d ${config.backups.borgDir}/.config/keys 0700 root root -"
-        "d ${config.backups.borgDir}/.config/security 0700 root root -"
-        "d ${config.backups.borgDir}/.tmp 0700 root root 2d"
-      ];
       services = {
-        "borgbackup-job-${config.networking.hostName}" = {
-          serviceConfig = {
-            ReadWritePaths = [
-              "${config.backups.borgDir}/.cache"
-              "${config.backups.borgDir}/.config"
-              "${config.backups.borgDir}/.tmp"
-            ];
-          };
-        };
         "cloudBackup" = {
           description = "rclone backup to backblaze cloud storage";
           serviceConfig = {
