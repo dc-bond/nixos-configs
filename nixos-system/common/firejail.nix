a30e19353795ac7b62f05579cfad8558200a2274
dc-bond <chris@dcbond.com>
2025-10-09 14:20:24 -0400

    .

--- nixos-system/common/firejail.nix
+++ nixos-system/common/firejail.nix
@@ -10,8 +10,17 @@
     enable = true;
     wrappedBinaries = {
       librewolf = {
-        executable = "${lib.getBin pkgs.librewolf}/bin/librewolf";
-        profile = "${pkgs.firejail}/etc/firejail/librewolf.profile";
+        #executable = "${lib.getBin pkgs.librewolf}/bin/librewolf";
+        executable = pkgs.writeShellScript "librewolf-ephemeral" ''
+          ${lib.getBin pkgs.librewolf}/bin/librewolf --private-window "$@"
+        '';
+        #profile = "${pkgs.firejail}/etc/firejail/librewolf.profile";
+        profile = pkgs.writeText "librewolf-ephemeral.profile" ''
+          include ${pkgs.firejail}/etc/firejail/librewolf.profile
+          tmpfs ~/.librewolf
+          tmpfs ~/.cache/librewolf
+          tmpfs ~/.local/share/librewolf
+        '';
       };
     };
   };
