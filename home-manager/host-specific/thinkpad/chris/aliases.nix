620d2d783f107f96287937a8f471f89e0f0587e2
dc-bond <chris@dcbond.com>
2025-10-10 19:34:39 -0400

    .

--- home-manager/host-specific/thinkpad/chris/aliases.nix
+++ home-manager/host-specific/thinkpad/chris/aliases.nix
@@ -10,9 +10,7 @@
   programs.zsh = {
 
     shellAliases = {
-      rb = "sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs#thinkpad --refresh";
       flakeupdate = "sudo nix flake update --flake ~/nixos-configs";
-      #garbage = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
       getnets = "iwctl station wlan0 get-networks";
       ledger = "cd /home/chris/nextcloud-client/Bond\\ Family/Financial/bond-ledger/ && nix develop";
       speed = "nix run nixpkgs#speedtest-rs";
