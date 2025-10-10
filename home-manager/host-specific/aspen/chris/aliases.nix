f41f023c9187269921ad5de965e9e0e566d9d78d
dc-bond <chris@dcbond.com>
2025-10-10 19:36:16 -0400

    .

--- home-manager/host-specific/aspen/chris/aliases.nix
+++ home-manager/host-specific/aspen/chris/aliases.nix
@@ -8,9 +8,7 @@
 
   programs.zsh = {
     shellAliases = {
-      rb = "sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs#aspen --refresh";
       storage = "cd /storage/WD-WCC7K4RU947F ; ls";
-      garbage = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
     };
   };
 
