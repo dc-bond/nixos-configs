754e3e2a7747172bc65bffeb7e40680f73bd8349
dc-bond <chris@dcbond.com>
2025-10-10 17:12:33 -0400

    working main 2025.10.10

--- nixos-system/common/login.nix
+++ nixos-system/common/login.nix
@@ -3,31 +3,21 @@
   ... 
 }: 
 
-#let
-#  greeter = "${pkgs.greetd.tuigreet}/bin/tuigreet";
-#  session = "${pkgs.hyprland}/bin/Hyprland";
-#  username = "chris";
-#in
+let
+  greeter = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+  session = "${pkgs.hyprland}/bin/Hyprland";
+  username = "chris";
+in
 
 {
 
-  #services.greetd = {
-  #  enable = true;
-  #  settings = {
-  #    default_session = {
-  #      command = "${greeter} --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --greeting 'access is restricted to authorized personnel' --asterisks --remember --remember-user-session --time -cmd ${session}";
-  #      user = "greeter";
-  #    };
-  #  };
-  #};
-
-
-  services = {
-    xserver.enable = true;
-    displayManager.sddm = {
-      enable = true;
-      wayland.enable = true;
-      settings.General.DefaultSession = "hyprland.desktop";
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${greeter} --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --greeting 'access is restricted to authorized personnel' --asterisks --remember --remember-user-session --time -cmd ${session}";
+        user = "greeter";
+      };
     };
   };
 
