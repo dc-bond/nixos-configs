8518574b2c3d92589d296d0cbfb4f6508612b8d6
dc-bond <chris@dcbond.com>
2025-10-12 12:57:05 -0400

    .

--- home-manager/common/aliases.nix
+++ home-manager/common/aliases.nix
@@ -8,8 +8,8 @@
 
   programs.zsh = {
     shellAliases = {
-      rbtest = "sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs/testing#$(hostname) --refresh";
       rbmain = "sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs#$(hostname) --refresh";
+      rbdev = "sudo nixos-rebuild switch --flake github:dc-bond/nixos-configs/dev#$(hostname) --refresh";
       garbage = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
     };
   };
