15268204081f261c8a498cdd214f202cd9974460
dc-bond <chris@dcbond.com>
2025-10-13 10:54:55 -0400

    .

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -6,7 +6,7 @@
 #firefox setup (activate extensions, etc.)
 #vscode github authentication
 #vscode extension setup
-#tailscale key?
+#tailscale auth key - remove old machine, generate new key in console, replace in secrets.yaml - key non-reusable, 90-day expiration, pre-authorized, non-ephemeral, update new tailscale IP in conficVars
 #email cloud recovery (sudo mkdir /var/lib/borgbackup/cloud-restore && sudo cloudRestore)
 
 { 
