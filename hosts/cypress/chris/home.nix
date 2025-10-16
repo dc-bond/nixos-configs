07b403612d440d24c7fb1ddd5aed4df5c159f19c
dc-bond <chris@dcbond.com>
2025-10-16 14:10:21 -0400

    .

--- hosts/cypress/chris/home.nix
+++ hosts/cypress/chris/home.nix
@@ -33,7 +33,35 @@
     zsh = {
       shellAliases = {
         storage = "cd /storage/WD-WX21DC86RU3P ; ls";
+        ledger = "cd /home/chris/nextcloud-client/Bond\\ Family/Financial/bond-ledger/ && nix develop";
+        speed = "nix run nixpkgs#speedtest-rs";
+        tsaspen = "sudo tailscale down && sleep 5 && sudo tailscale up -ssh --accept-routes --exit-node=${configVars.aspenTailscaleIp}";
+        tsjuniper = "sudo tailscale down && sleep 5 && sudo tailscale up -ssh --accept-routes --exit-node=${configVars.juniperTailscaleIp}";
       };
+      initContent = ''
+
+        reconnect-mouse() {
+          echo "restarting bluetooth service..."
+          sudo systemctl restart bluetooth
+          sleep 3
+          
+          echo "power cycling bluetooth..."
+          bluetoothctl power off
+          sleep 2
+          bluetoothctl power on
+          sleep 3
+          
+          echo "reconnecting mouse..."
+          bluetoothctl connect D3:CF:05:5D:88:79
+          echo "Bluetooth reconnection complete!"
+        }
+
+        librewolf-private() {
+          echo "launching LibreWolf..."
+          librewolf --private-window "https://ipleak.net" "$@"
+        }
+
+      '';
     };
   };
 
