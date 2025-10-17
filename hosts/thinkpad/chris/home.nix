e0f4fcdf1a06aeab0560259f1c67c0699f2be66a
dc-bond <chris@dcbond.com>
2025-10-17 13:21:56 -0400

    .

--- hosts/thinkpad/chris/home.nix
+++ hosts/thinkpad/chris/home.nix
@@ -23,7 +23,7 @@
       "home-manager/chris/common/email.nix"
 
       #"home-manager/chris/host-specific/thinkpad/hyprland.nix"
-      #"home-manager/chris/host-specific/thinkpad/plasma.nix"
+      "home-manager/chris/host-specific/thinkpad/plasma.nix"
     ])
   ];
 
