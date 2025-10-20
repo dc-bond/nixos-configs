e17be91c0c6f8a0b5a663e76f5fd05f3d762bdf2
dc-bond <chris@dcbond.com>
2025-10-20 17:35:06 -0400

    .

--- nixos-system/tailscale.nix
+++ nixos-system/tailscale.nix
@@ -34,9 +34,9 @@ in
       "-ssh"
     ] 
     ++ lib.optionals isServer [ "--advertise-exit-node" ]
+    ++ lib.optionals isClient [ "--accept-routes" "--exit-node=${configVars.aspenTailscaleIp}" ]
     ++ lib.optional (hostname == "aspen") "--advertise-routes=192.168.1.0/24,192.168.4.0/27"
-    ++ lib.optionals isClient [ "--accept-routes" "--exit-node=${configVars.aspenTailscaleIp}" ];
-    #++ lib.optional (hostname == "thinkpad") "--exit-node=${configVars.aspenTailscaleIp}"; # thinkpad laptop (client) always needs to default to using server exit node (aspen or juniper)
+    ++ lib.optional (hostname == "thinkpad") "--exit-node=${configVars.aspenTailscaleIp}"; # thinkpad laptop (client) always needs to default to using server exit node (aspen or juniper)
   };
 
 }
