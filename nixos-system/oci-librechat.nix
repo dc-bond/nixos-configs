a000d465c44d47c8b2e013e2a5a8adee21c953bf
dc-bond <chris@dcbond.com>
2025-10-23 07:38:57 -0400

    .

--- nixos-system/oci-librechat.nix
+++ nixos-system/oci-librechat.nix
@@ -182,7 +182,7 @@ in
           #============#
           
           ANTHROPIC_API_KEY=${config.sops.placeholder.librechatAnthropicApiKey}
-          ANTHROPIC_MODELS=claude-haiku-4-5-20251001,claude-sonnet-4-5-20250929
+          ANTHROPIC_MODELS=claude-haiku-4-5-20251001,claude-sonnet-4-5-20250929,claude-opus-4-1-20250805
           
           #============#
           # OpenAI     #
