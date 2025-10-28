31be1ab93602379ab7b28bab401e1317ed394b4c
dc-bond <chris@dcbond.com>
2025-10-28 14:31:52 -0400

    .

--- nixos-system/plasma.nix
+++ nixos-system/plasma.nix
@@ -50,6 +50,7 @@
         #auto_login_service = "ly-autologin";
         #auto_login_user = "chris";
         #auto_login_session = "plasma";
+        waylandsessions = "/run/current-system/sw/share/wayland-sessions";
         auth_fails = 3;
         save = true;
       };
