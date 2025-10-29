e68270ba0390a67974dca11f34baa39452d44053
dc-bond <chris@dcbond.com>
2025-10-29 14:07:47 -0400

    .

--- nixos-system/bluetooth.nix
+++ nixos-system/bluetooth.nix
@@ -9,4 +9,15 @@
     powerOnBoot = true;
   };
 
+  systemd.services.bluetooth-power-on = {
+    description = "Power on Bluetooth adapter";
+    after = [ "bluetooth.service" ];
+    requires = [ "bluetooth.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.bluez}/bin/bluetoothctl power on";
+    };
+  };
+
 }
