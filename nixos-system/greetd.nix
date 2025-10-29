4dd5c4c1ea73600ddb6ba8326d59066b65289db4
dc-bond <chris@dcbond.com>
2025-10-29 19:29:07 -0400

    .

--- nixos-system/greetd.nix
+++ nixos-system/greetd.nix
@@ -23,20 +23,32 @@ in
     enable = true;
     settings = {
       default_session.command = ''
-        ${pkgs.greetd.tuigreet}/bin/tuigreet \
-          --time \
-          --time-format '%a, %d %b %Y • %H:%M' \
-          --asterisks \
-          --theme "border=blue;text=white;prompt=cyan;time=green;action=green;button=yellow" \
-          --greeting "Access is restricted to authorized personnel only." \
-          --user-menu \
-          --remember \
-          --remember-user-session \
-          --sessions ${customSessions}/share/wayland-sessions:/run/current-system/sw/share/wayland-sessions \
-          --xsessions /dev/null \
-          --cmd startplasma-wayland
+        command = "${pkgs.cage}/bin/cage -s -m last -- ${pkgs.greetd.regreet}/bin/regreet";
+        user = "greeter";
       '';
     };
   };
 
+  programs.regreet.enable = true;
+
 }
+
+  #services.greetd = {
+  #  enable = true;
+  #  settings = {
+  #    default_session.command = ''
+  #      ${pkgs.greetd.tuigreet}/bin/tuigreet \
+  #        --time \
+  #        --time-format '%a, %d %b %Y • %H:%M' \
+  #        --asterisks \
+  #        --theme "border=blue;text=white;prompt=cyan;time=green;action=green;button=yellow" \
+  #        --greeting "Access is restricted to authorized personnel only." \
+  #        --user-menu \
+  #        --remember \
+  #        --remember-user-session \
+  #        --sessions ${customSessions}/share/wayland-sessions:/run/current-system/sw/share/wayland-sessions \
+  #        --xsessions /dev/null \
+  #        --cmd startplasma-wayland
+  #    '';
+  #  };
+  #};
