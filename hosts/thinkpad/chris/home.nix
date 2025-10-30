237fc96217e01fd61b3a27f42877ebcea203a009
dc-bond <chris@dcbond.com>
2025-10-30 06:42:28 -0400

    .

--- hosts/thinkpad/chris/home.nix
+++ hosts/thinkpad/chris/home.nix
@@ -43,15 +43,21 @@
   xdg.userDirs = {
     enable = true;
     createDirectories = false;
-    download = "${config.home.homeDirectory}/Downloads";
-    documents = "${config.home.homeDirectory}/Documents";
+    download = "${config.home.homeDirectory}/downloads";
+    documents = "${config.home.homeDirectory}/documents";
     desktop = null;
   };
+
+  # ensure nextcloud-client directory exists
+  systemd.user.tmpfiles.rules = [
+    "d %h/nextcloud-client 0755 - - -"
+    "d %h/test 0755 - - -"
+  ];
 
 # start/re-start services after system rebuild
   systemd.user.startServices = "sd-switch";
 
 # original home state version - defines the first version of home-manager installed to maintain compatibility with application data (e.g. databases) created on older versions that can't automatically update their data when their package is updated
-  home.stateVersion = "23.11";
+  home.stateVersion = "25.05";
 
 }
--- nixos-system/oci-librechat.nix
+++ nixos-system/oci-librechat.nix
@@ -188,7 +188,7 @@ in
           #============#
           
           ANTHROPIC_API_KEY=${config.sops.placeholder.librechatAnthropicApiKey}
-          ANTHROPIC_MODELS=claude-haiku-4-5-20251001,claude-sonnet-4-5-20250929,claude-opus-4-1-20250805
+          ANTHROPIC_MODELS=claude-sonnet-4-5-20250929,claude-opus-4-1-20250805
           
           #============#
           # OpenAI     #
