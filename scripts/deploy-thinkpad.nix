50c6cc2ecb719494b847cb8b1a9a2d14ef827a83
dc-bond <chris@dcbond.com>
2025-10-30 20:27:40 -0400

    .

--- scripts/deploy-thinkpad.nix
+++ scripts/deploy-thinkpad.nix
@@ -10,6 +10,7 @@ let
 
   HOST = "thinkpad";
   IPV4 = "192.168.1.62";
+  USER1 = "chris";
   
   deployThinkpadScript = pkgs.writeShellScriptBin "deployThinkpad" ''
     #!/usr/bin/env bash
@@ -26,19 +27,26 @@ let
     # create directory where sops expects to find the age host key
     install -d -m755 "$temp/etc/age"
     
-    # decrypt private key from the password store and copy it to the temporary directory
+    # decrypt private system key from password store and copy to temp directory
     pass hosts/${HOST}/age/private > "$temp/etc/age/${HOST}-age.key"
     
     # set the correct permissions
     chmod 600 "$temp/etc/age/${HOST}-age.key"
 
+    # create directory where sops expects to find age user key
+    install -d -m700 "$temp/home/${USER1}/.config/age"
+    
+    # decrypt private user key from password store and copy to temp directory
+    pass users/${USER1}/age/private > "$temp/home/${USER1}/.config/age/${USER1}-age.key"
+    chmod 600 "$temp/home/${USER1}/.config/age/${USER1}-age.key"
+
     # move to correct directory to generate hardware-configuration.nix
-    cd /home/chris/nixos-configs/hosts/${HOST}
+    cd /home/${USER1}/nixos-configs/hosts/${HOST}
 
     # install
     nix run github:nix-community/nixos-anywhere -- \
     --generate-hardware-config nixos-generate-config ./hardware-configuration.nix \
-    --disk-encryption-keys /tmp/crypt-passwd.txt <(pass users/chris/passwd) \
+    --disk-encryption-keys /tmp/crypt-passwd.txt <(pass users/${USER1}/passwd) \
     --extra-files "$temp" \
     --flake '.#${HOST}' \
     root@${IPV4}
