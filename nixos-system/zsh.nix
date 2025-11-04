82d2cadde2e568e8ac206924b5125b25c08ba3a6
dc-bond <chris@dcbond.com>
2025-11-04 13:18:43 -0500

    .

--- nixos-system/zsh.nix
+++ nixos-system/zsh.nix
@@ -9,8 +9,17 @@
     enable = true; # z-shell enabled system-wide to source necessary files for users
     # function to tail systemd logs
     interactiveShellInit = '' 
+      # function to tail systemd logs for a specific service
       jlog() {
         journalctl -e -u "$1" --since "''${2:-1 day ago}" --no-pager --follow
+      }
+      # function to show status of all fail2ban jails
+      f2bstats() {
+        for jail in $(sudo fail2ban-client status | grep "Jail list" | sed 's/.*://;s/,//g'); do
+          echo "=== $jail ==="
+          sudo fail2ban-client status $jail
+          echo
+        done
       }
     '';
   };
