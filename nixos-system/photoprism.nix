455b63de8a6d90eef3bbd9a018f7339daa50f357
dc-bond <chris@dcbond.com>
2025-11-20 07:55:04 -0500

    .

--- nixos-system/photoprism.nix
+++ nixos-system/photoprism.nix
@@ -43,7 +43,10 @@ in
     borgCryptPasswd = {};
   };
 
-  environment.systemPackages = with pkgs; [ recoverScript ];
+  environment.systemPackages = with pkgs; [ 
+    recoverScript
+    vips # for using vips to generate thumbnails
+  ];
 
   systemd.services."${app}" = {
     requires = [ "mysql.service" ];
@@ -69,22 +72,33 @@ in
       originalsPath = "${config.hostSpecificConfigs.storageDrive1}/media/family-media";
       passwordFile = "${config.sops.secrets.photoprismAdminPasswd.path}";
       settings = {
-        PHOTOPRISM_AUTH_MODE = "password";                                                      # authentication mode (public, password)
+        PHOTOPRISM_AUTH_MODE = "public";                                                        # authentication mode (public, password)
         PHOTOPRISM_SITE_URL = "https://photos.${configVars.domain2}/";                          # public server URL incl http:// or https:// and /path, :port is optional
-        PHOTOPRISM_ORIGINALS_LIMIT = "20000";                                                   # file size limit for originals in MB (increase for high-res video)
+        PHOTOPRISM_ORIGINALS_LIMIT = "50000";                                                   # file size limit for originals in MB (increase for high-res video)
         PHOTOPRISM_HTTP_COMPRESSION = "gzip";                                                   # improves transfer speed and bandwidth utilization (none or gzip)
         PHOTOPRISM_LOG_LEVEL = "info";                                                          # log level: trace, debug, info, warning, error, fatal, or panic
         PHOTOPRISM_READONLY = "false";                                                          # do not modify originals directory (reduced functionality)
         PHOTOPRISM_EXPERIMENTAL = "false";                                                      # enables experimental features
         PHOTOPRISM_DISABLE_CHOWN = "false";                                                     # disables updating storage permissions via chmod and chown on startup
         PHOTOPRISM_DISABLE_WEBDAV = "false";                                                    # disables built-in WebDAV server
-        PHOTOPRISM_DISABLE_SETTINGS = "false";                                                  # disables settings UI and API
+        PHOTOPRISM_DISABLE_SETTINGS = "true";                                                   # disables settings UI and API in "public" auth mode
         PHOTOPRISM_DISABLE_TENSORFLOW = "true";                                                 # disables all features depending on TensorFlow
+        PHOTOPRISM_DISABLE_RAWTHERAPEE = "true";
+        PHOTOPRISM_DISABLE_DARKTABLE = "true";
+        PHOTOPRISM_DISABLE_IMAGEMAGICK = "true";
         PHOTOPRISM_DISABLE_FACES = "true";                                                      # disables face detection and recognition (requires TensorFlow)
         PHOTOPRISM_DISABLE_CLASSIFICATION = "true";                                             # disables image classification (requires TensorFlow)
         PHOTOPRISM_DISABLE_RAW = "false";                                                       # disables indexing and conversion of RAW files
         PHOTOPRISM_RAW_PRESETS = "false";                                                       # enables applying user presets when converting RAW files (reduces performance)
-        PHOTOPRISM_JPEG_QUALITY = "100";                                                        # a higher value increases the quality and file size of JPEG images and thumbnails (25-100)
+        PHOTOPRISM_JPEG_QUALITY = "95";                                                         # a higher value increases the quality and file size of JPEG images and thumbnails (25-100)
+        #PHOTOPRISM_JPEG_SIZE = "2560";                                                         # size of JPEG when converting from RAW
+        #PHOTOPRISM_PNG_SIZE = "2560";                                                          # size of PNG when converting from RAW
+        PHOTOPRISM_THUMB_UNCACHED = "true";                                                     # dynamically generates thumbs while scrolling through library
+        PHOTOPRISM_THUMB_SIZE_UNCACHED = "2560";
+        #PHOTOPRISM_THUMB_SIZE = "2560";                                                        # used when THUMB_UNCACHED is set to false; pre-generates thumbs at this pixel density
+        PHOTOPRISM_THUMB_FILTER = "blackman";
+        PHOTOPRISM_THUMB_COLOR = "srgb";
+        PHOTOPRISM_THUMB_LIBRARY = "vips";
         PHOTOPRISM_DETECT_NSFW = "false";                                                       # automatically flags photos as private that MAY be offensive (requires TensorFlow)
         PHOTOPRISM_UPLOAD_NSFW = "true";                                                        # allows uploads that MAY be offensive (no effect without TensorFlow)
         PHOTOPRISM_DATABASE_DRIVER = "mysql";                                                   # use MariaDB 10.5+ or MySQL 8+ instead of SQLite for improved performance
@@ -94,9 +108,8 @@ in
         PHOTOPRISM_SITE_CAPTION = "Bond Private Family Photo and Video Server";
         PHOTOPRISM_SITE_DESCRIPTION = "Bond Family Photos and Videos";
         PHOTOPRISM_SITE_AUTHOR = "${configVars.chrisFullName}";
-        PHOTOPRISM_THUMB_UNCACHED = "true";
         NVIDIA_VISIBLE_DEVICES = "all";
-        PHOTOPRISM_INDEX_SCHEDULE = "";
+        PHOTOPRISM_INDEX_SCHEDULE = "0 6 * * *";                                                # auto-index daily at 6am
       };
     };
 
