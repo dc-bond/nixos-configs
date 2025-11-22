9a4932ef0f9671262db9e56a5f0320cf5b537c91
dc-bond <chris@dcbond.com>
2025-11-22 11:05:24 -0500

    .

---
+++ hosts/alder/chris/home.nix
@@ -0,0 +1,49 @@
+{ 
+  inputs, 
+  config,
+  lib,
+  configLib,
+  configVars,
+  pkgs, 
+  ... 
+}: 
+
+{
+  
+  imports = lib.flatten [
+    (map configLib.relativeToRoot [
+      "home-manager/chris/zsh.nix"
+      "home-manager/chris/starship.nix"
+      "home-manager/chris/neovim.nix"
+    ])
+  ];
+
+  programs.home-manager.enable = true; # enable home manager
+
+# define username and home directory
+  home = {
+    username = configVars.chrisUsername;
+    homeDirectory = "/home/${configVars.chrisUsername}";
+  };
+
+# define default folders in home directory
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = false;
+    download = "${config.home.homeDirectory}/downloads";
+    documents = "${config.home.homeDirectory}/documents";
+    desktop = null;
+  };
+
+  ## ensure nextcloud-client directory exists
+  #systemd.user.tmpfiles.rules = [
+  #  "d %h/nextcloud-client 0755 - - -"
+  #];
+
+# start/re-start services after system rebuild
+  systemd.user.startServices = "sd-switch";
+
+# original home state version - defines the first version of home-manager installed to maintain compatibility with application data (e.g. databases) created on older versions that can't automatically update their data when their package is updated
+  home.stateVersion = "25.05";
+
+}
