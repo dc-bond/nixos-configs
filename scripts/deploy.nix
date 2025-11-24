c6dd1af62c1b5176c996eb3d76ea028ee137a45e
dc-bond <chris@dcbond.com>
2025-11-24 10:24:20 -0500

    .

--- scripts/deploy.nix
+++ scripts/deploy.nix
@@ -10,14 +10,14 @@
 }:
 
 let
-  # Generate deployment script for a specific host
+  # generate deployment script for a specific host
   mkDeployScript = hostname: hostConfig:
     let
       users = hostConfig.users;
       ipv4 = hostConfig.ipv4;
       useDiskEncryption = hostConfig.diskEncryption or false;
       
-      # Generate age key setup commands for all users
+      # generate age key setup commands for all users
       userAgeSetup = lib.concatMapStringsSep "\n" (user: ''
         # Setup age key for ${user}
         install -d -m700 "$temp/home/${user}/.config/age"
@@ -32,26 +32,26 @@ let
       echo "Deploying ${hostname} to ${ipv4}..."
       ${lib.optionalString useDiskEncryption ''echo "Using disk encryption for this host..."''}
       
-      # Create a temporary directory
+      # create a temporary directory
       temp=$(mktemp -d)
       trap "rm -rf $temp" EXIT
       
-      # Setup system age key
+      # setup system age key
       install -d -m755 "$temp/etc/age"
       pass hosts/${hostname}/age/private > "$temp/etc/age/${hostname}-age.key"
       chmod 600 "$temp/etc/age/${hostname}-age.key"
       
       ${userAgeSetup}
       
-      # Move to host directory
+      # move to host directory
       cd "$HOME/nixos-configs/hosts/${hostname}"
       
-      # Build the nixos-anywhere command
+      # build the nixos-anywhere command
       nix run github:nix-community/nixos-anywhere -- \
         --generate-hardware-config nixos-generate-config ./hardware-configuration.nix \
         ${lib.optionalString useDiskEncryption ''--disk-encryption-keys /tmp/crypt-passwd.txt <(pass hosts/${hostname}/disk-encryption-passwd) \''}
         --extra-files "$temp" \
-        ${lib.concatMapStringsSep " \\\n        " (user: 
+        ${lib.concatMapStringsSep " \\\n  " (user: 
           ''--chown /home/${user} ${toString configVars.users.${user}.uid}:100''
         ) users} \
         --flake '.#${hostname}' \
