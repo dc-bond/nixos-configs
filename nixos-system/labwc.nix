5d15fe8d01c67be4318e8cd27d21947b16c26645
dc-bond <chris@dcbond.com>
2025-11-27 10:04:34 -0500

    .

--- nixos-system/labwc.nix
+++ nixos-system/labwc.nix
@@ -8,8 +8,28 @@
 
 {
 
-  programs.labwc = {
-    enable = true;
+  environment.systemPackages = with pkgs; [
+    wayland
+  ];
+
+  programs = {
+    wayland.enable = true;
+    labwc.enable = true;
   };
+
+  # create session file so tuigreet can discover and launch labwc
+  services.displayManager.sessionPackages = [ 
+    (pkgs.writeTextFile {
+      name = "labwc-session";
+      destination = "/share/wayland-sessions/labwc.desktop";
+      text = ''
+        [Desktop Entry]
+        Name=labwc
+        Comment=Stacking Wayland compositor
+        Exec=${pkgs.labwc}/bin/labwc
+        Type=Application
+      '';
+    })
+  ];
 
 }
