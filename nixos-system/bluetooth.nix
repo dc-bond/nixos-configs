f995fa749060f44d204e5a606a6285ee640c6edd
dc-bond <chris@dcbond.com>
2026-03-31 13:10:17 -0400

    update bluetooth

--- nixos-system/bluetooth.nix
+++ nixos-system/bluetooth.nix
@@ -1,7 +1,7 @@
-{ 
-  pkgs, 
-  ... 
-}: 
+{
+  pkgs,
+  ...
+}:
 
 {
 
@@ -9,11 +9,19 @@
     enable = true; # also installs bluez-utils to provide bluetoothctl terminal application
     powerOnBoot = true;
     settings = {
+      General = {
+        FastConnectable = true; # faster reconnection for paired devices (uses more power)
+      };
       Policy = {
         AutoEnable = true;
       };
     };
   };
+
+  # disable USB autosuspend for bluetooth adapters - prevents sleep-induced reconnection delays
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{bInterfaceClass}=="e0", ATTR{bInterfaceSubClass}=="01", TEST=="power/control", ATTR{power/control}="on"
+  '';
 
   #services.blueman.enable = true; # gui application
 
