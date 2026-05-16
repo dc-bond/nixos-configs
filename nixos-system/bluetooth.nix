8811d3591a6caadcef1c9d368b3b80dc4aa66f2d
dc-bond <chris@dcbond.com>
2026-05-16 07:35:46 -0400

    bluetooth fix

--- nixos-system/bluetooth.nix
+++ nixos-system/bluetooth.nix
@@ -11,14 +11,14 @@
     settings = {
       General = {
         FastConnectable = true; # faster reconnection for paired devices (uses more power)
-        ReconnectAttempts = 7; # increase from default 3
-        ReconnectIntervals = "1,2,4,8,16,32,64"; # backoff intervals in seconds
         # enable LL privacy for hardware-accelerated address resolution (faster BLE reconnection)
         # https://github.com/bluez/bluez/blob/master/src/main.conf
         KernelExperimental = "15c0a148-c273-11ea-b3de-0242ac130004";
       };
       Policy = {
         AutoEnable = true;
+        ReconnectAttempts = 7; # increase from default 3
+        ReconnectIntervals = "1,2,4,8,16,32,64"; # backoff intervals in seconds
       };
     };
   };
