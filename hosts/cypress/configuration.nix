167f28c053dce55520ffea56316d82bf83b5675e
dc-bond <chris@dcbond.com>
2026-06-01 15:32:07 -0400

    hermes smoke test

--- hosts/cypress/configuration.nix
+++ hosts/cypress/configuration.nix
@@ -109,7 +109,7 @@
 
   imports = lib.flatten [
     inputs.disko.nixosModules.disko
-    #inputs.private.nixosModules.nspawn-hermes
+    inputs.private.nixosModules.nspawn-hermes
     (map configLib.relativeToRoot [
       "hosts/cypress/hardware-configuration.nix"
       "hosts/cypress/impermanence.nix"
